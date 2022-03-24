2bc071102b974d1153a038b73720d8616526aed7
dongzezhao <dongzezhao@baidu.com>
2022-03-24 10:13:02 +0800

    update queue api for paddleflow client

--- client/paddleflow/cli/queue.py
+++ client/paddleflow/cli/queue.py
@@ -77,35 +77,61 @@ def show(ctx, queuename):
 @queue.command(context_settings=dict(max_content_width=2000), cls=command_required_option_from_option())
 @click.argument('name')
 @click.argument('namespace')
-@click.argument('cpu')
-@click.argument('mem')
 @click.argument('clustername')
+@click.argument('maxcpu')
+@click.argument('maxmem')
+@click.option('--maxscalar', help='the max scalar resource of queue, --maxscalar a=b,c=d')
+@click.option('--mincpu', help='the min cpu resource of queue')
+@click.option('--minmem', help='the min memory resource of queue')
+@click.option('--minscalar', help='the min scalar resource of queue, --minscalar a=b,c=d')
+@click.option('--policy', help='the scheduling policy for job on queue')
+@click.option('-l', '--location', help='the location of queue')
+@click.option('--quota', help='the quota type of queue, such as elasticQuota, volcanoCapabilityQuota.')
 @click.pass_context
-def create(ctx, name, namespace, cpu, mem, clustername):
+def create(ctx, name, namespace, clustername, maxcpu, maxmem, maxscalar=None, mincpu=None, minmem=None, minscalar=None,
+            policy=None, location=None, quota=None):
     """ create queue.\n
     NAME: the name of queue.
-    NAMESPACE: The namespace to which it belongs.
-    CPU: CPU occupied by queue.
-    MEN: Men occupied by queue.
-    CLUSTERNAME: cluster name.
+    NAMESPACE: the namespace to which it belongs.
+    CLUSTERNAME: the cluster name.
+    MAXCPU: the max CPU occupied by queue.
+    MAXMEM: the max Memory occupied by queue.
     """
     client = ctx.obj['client']
     if not name:
-        click.echo('queue create  must provide name.', err=True)
+        click.echo('queue create must provide name.', err=True)
         sys.exit(1)
     if not namespace:
-        click.echo('queue create  must provide namespace.', err=True)
-        sys.exit(1)
-    if not cpu:
-        click.echo('queue create  must provide cpu.', err=True)
-        sys.exit(1)
-    if not mem:
-        click.echo('queue create  must provide mem.', err=True)
+        click.echo('queue create must provide namespace.', err=True)
         sys.exit(1)
     if not clustername:
-        click.echo('queue create  must provide clustername.', err=True)
+        click.echo('queue create must provide clustername.', err=True)
         sys.exit(1)
-    valid, response = client.add_queue(name, namespace, cpu, mem, clustername)
+    if not maxcpu:
+        click.echo('queue create must provide maxcpu.', err=True)
+        sys.exit(1)
+    if not maxcpu:
+        click.echo('queue create must provide maxmem.', err=True)
+        sys.exit(1)
+    maxresources = {
+        'cpu': maxcpu,
+        'mem': maxmem,
+    }
+    if maxscalar:
+        args = maxscalar.split(',')
+        maxresources['scalarResources'] = dict([item.split('=') for item in args])
+    minresources = None
+    if mincpu and minmem:
+        minresources = {
+            'cpu': mincpu,
+            "mem": minmem,
+        }
+    if minscalar:
+        args = minscalar.split(',')
+        minresources['scalarResources'] = dict([item.split('=') for item in args])
+
+    valid, response = client.add_queue(name, namespace, clustername, maxresources, minresources,
+                                       policy, location, quota)
     if valid:
         click.echo("queue[%s] create success " % name)
     else:
@@ -216,17 +242,25 @@ def ungrant(ctx, username, queuename):
 
 def _print_queues(queues, out_format):
     """print queues """
-    headers = ['name', 'status', 'create time', 'update time']
-    data = [[queue.name, queue.status, queue.createTime, queue.updateTime] for queue in queues]
+    headers = ['name', 'namespace', 'status', 'cluster name', 'create time', 'update time']
+    data = [[queue.name, queue.namespace, queue.status, queue.clusterName,
+             queue.createTime, queue.updateTime] for queue in queues]
     print_output(data, headers, out_format, table_format='grid')
 
 
 def _print_queue_info(queue, out_format):
     """print queue info"""
-    headers = ['name', 'status', 'namespace', 'mem', 'cpu', 'cluster name', 'create time', 'update time']
-    data = [[queue.name, queue.status, queue.namespace, queue.mem, queue.cpu, queue.clustername or None,
-            queue.createTime, queue.updateTime]]
+    headers = ['name', 'namespace', 'status', 'cluster name', 'create time', 'update time']
+    data = [[queue.name, queue.namespace, queue.status, queue.clusterName, queue.createTime, queue.updateTime]]
     print_output(data, headers, out_format, table_format='grid')
+    if queue.maxResources:
+        print_output([[queue.maxResources]], ['max resources'], out_format, table_format='grid')
+    if queue.minResources:
+        print_output([[queue.minResources]], ['min resources'], out_format, table_format='grid')
+    if queue.schedulingPolicy:
+        print_output([[queue.schedulingPolicy]], ['scheduling policy'], out_format, table_format='grid')
+    if queue.location:
+        print_output([[queue.location]], ['location'], out_format, table_format='grid')
 
 
 def _print_grants(grants, out_format):
