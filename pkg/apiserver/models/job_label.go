6f66034eb3f6881e43370588d821195ec205fa9e
lidongbo01 <lidongbo01@baidu.com>
2022-03-30 20:02:38 +0800

    job design

--- pkg/apiserver/models/job_label.go
+++ pkg/apiserver/models/job_label.go
@@ -8,7 +8,7 @@ import (
 
 type JobLabel struct {
 	Pk        int64  `gorm:"primaryKey;autoIncrement"`
-	ID        string `json:"-" gorm:"type:varchar(36);uniqueIndex"`
+	ID        string `gorm:"type:varchar(36);uniqueIndex"`
 	Label     string `gorm:"type:varchar(255);NOT NULL"`
 	JobID     string `gorm:"type:varchar(60);NOT NULL"`
 	CreatedAt time.Time
