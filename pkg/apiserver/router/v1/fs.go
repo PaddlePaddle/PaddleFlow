f260aec63eb194b0d6d6dc868786da3886d71f0a
luoyuedong <luoyuedong@baidu.com>
2022-04-11 15:50:24 +0800

    fix fs list

--- pkg/apiserver/router/v1/fs.go
+++ pkg/apiserver/router/v1/fs.go
@@ -372,11 +372,10 @@ func checkFsDir(fsType, url string, properties map[string]string) error {
 func (pr *PFSRouter) ListFileSystem(w http.ResponseWriter, r *http.Request) {
 	ctx := common.GetRequestContext(r)
 
-	var maxKeys int
-	if r.URL.Query().Get(util.QueryKeyMaxKeys) == "" {
-		maxKeys = util.DefaultMaxKeys
-	} else {
-		maxKeys, _ = strconv.Atoi(r.URL.Query().Get(util.QueryKeyMaxKeys))
+	maxKeys, err := util.GetQueryMaxKeys(&ctx, r)
+	if err != nil {
+		common.RenderErrWithMessage(w, ctx.RequestID, common.InvalidURI, err.Error())
+		return
 	}
 	listRequest := &api.ListFileSystemRequest{
 		FsName:   r.URL.Query().Get(util.QueryFsName),
