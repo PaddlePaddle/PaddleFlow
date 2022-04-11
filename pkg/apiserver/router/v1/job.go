1caa1c87d99383c18ddaed763596a39e7d9dd23e
zhongzichao <zhongzichao@baidu.com>
2022-04-11 20:58:28 +0800

    add single job api

---
+++ pkg/apiserver/router/v1/job.go
@@ -0,0 +1,204 @@
+/*
+Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+
+	"paddleflow/pkg/apiserver/common"
+	"paddleflow/pkg/apiserver/controller/job"
+	"paddleflow/pkg/common/config"
+	"paddleflow/pkg/common/logger"
+)
+
+// JobRouter is job api router
+type JobRouter struct{}
+
+// Name indicate name of job router
+func (jr *JobRouter) Name() string {
+	return "JobRouter"
+}
+
+// AddRouter add job router to root router
+func (jr *JobRouter) AddRouter(r chi.Router) {
+	log.Info("add job router")
+	r.Post("/job/single", jr.CreateSingleJob)
+	r.Post("/job/distributed", jr.CreateDistributedJob)
+	r.Post("/job/workflow", jr.CreateWorkflowJob)
+}
+
+// CreateSingleJob create single job
+// @Summary 创建single类型作业
+// @Description 创建single类型作业
+// @Id createSingleJob
+// @tags User
+// @Accept  json
+// @Produce json
+// @Success 200 {object} job.CreateJobResponse "创建single类型作业的响应"
+// @Failure 400 {object} common.ErrorResponse "400"
+// @Router /job/single [POST]
+func (jr *JobRouter) CreateSingleJob(w http.ResponseWriter, r *http.Request) {
+	ctx := common.GetRequestContext(r)
+
+	var request job.CreateSingleJobRequest
+	if err := common.BindJSON(r, &request); err != nil {
+		ctx.ErrorCode = common.MalformedJSON
+		logger.LoggerForRequest(&ctx).Errorf("parsing request body failed:%+v. error:%s", r.Body, err.Error())
+		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
+		return
+	}
+
+	request.CommonJobInfo.UserName = ctx.UserName
+
+	// validate Job
+	if err := validateSingleJob(&ctx, &request); err != nil {
+		ctx.Logging().Errorf("validate job request failed. request:%v error:%s", request, err.Error())
+		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
+		return
+	}
+
+	response, err := job.CreateSingleJob(&request)
+	if err != nil {
+		ctx.Logging().Errorf("create job failed. job request:%v error:%s", request, err.Error())
+		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
+		return
+	}
+	ctx.Logging().Debugf("CreateJob job:%v", string(config.PrettyFormat(response)))
+	common.Render(w, http.StatusOK, response)
+}
+
+// CreateDistributedJob create distributed job
+// @Summary 创建Distributed类型作业
+// @Description 创建Distributed类型作业
+// @Id createDistributedJob
+// @tags User
+// @Accept  json
+// @Produce json
+// @Success 200 {object} job.CreateJobResponse "创建single类型作业的响应"
+// @Failure 400 {object} common.ErrorResponse "400"
+// @Router /job/distributed [POST]
+func (jr *JobRouter) CreateDistributedJob(w http.ResponseWriter, r *http.Request) {
+	ctx := common.GetRequestContext(r)
+
+	var request job.CreateDisJobRequest
+	if err := common.BindJSON(r, &request); err != nil {
+		ctx.ErrorCode = common.MalformedJSON
+		logger.LoggerForRequest(&ctx).Errorf("parsing request body failed:%+v. error:%s", r.Body, err.Error())
+		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
+		return
+	}
+
+	request.CommonSpec.UserName = ctx.UserName
+
+	// validate Job
+	if err := validateDistributedJob(&ctx, &request); err != nil {
+		ctx.Logging().Errorf("validate job request failed. request:%v error:%s", request, err.Error())
+		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
+		return
+	}
+
+	response, err := job.CreateDistributedJob(&request)
+	if err != nil {
+		ctx.Logging().Errorf("create job failed. job request:%v error:%s", request, err.Error())
+		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
+		return
+	}
+	ctx.Logging().Debugf("CreateJob job:%v", string(config.PrettyFormat(response)))
+	common.Render(w, http.StatusOK, response)
+}
+
+// CreateWorkflowJob create workflow job
+// @Summary 创建Workflow类型作业
+// @Description 创建Workflow类型作业
+// @Id createWorkflowJob
+// @tags User
+// @Accept  json
+// @Produce json
+// @Success 200 {object} job.CreateJobResponse "创建single类型作业的响应"
+// @Failure 400 {object} common.ErrorResponse "400"
+// @Router /job/workflow [POST]
+func (jr *JobRouter) CreateWorkflowJob(w http.ResponseWriter, r *http.Request) {
+	ctx := common.GetRequestContext(r)
+
+	var request job.CreateWfJobRequest
+	if err := common.BindJSON(r, &request); err != nil {
+		ctx.ErrorCode = common.MalformedJSON
+		logger.LoggerForRequest(&ctx).Errorf("parsing request body failed:%+v. error:%s", r.Body, err.Error())
+		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
+		return
+	}
+
+	if err := validateWorkflowJob(&ctx, &request); err != nil {
+		ctx.Logging().Errorf("validate job request failed. request:%v error:%s", request, err.Error())
+		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
+		return
+	}
+
+	response, err := job.CreateWorkflowJob(&request)
+	if err != nil {
+		ctx.Logging().Errorf("create job failed. job request:%v error:%s", request, err.Error())
+		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
+		return
+	}
+	ctx.Logging().Debugf("CreateJob job:%v", string(config.PrettyFormat(response)))
+	common.Render(w, http.StatusOK, response)
+}
+
+func validateSingleJob(ctx *logger.RequestContext, request *job.CreateSingleJobRequest) error {
+	// ensure required fields
+	emptyFields := validateEmptyField(request)
+	if len(emptyFields) != 0 {
+		emptyFieldStr := strings.Join(emptyFields, ",")
+		err := fmt.Errorf("required fields in {%s} are empty, please fill it", emptyFieldStr)
+		ctx.Logging().Errorf("create single job failed. error: %s", err.Error())
+		ctx.ErrorCode = common.RequiredFieldEmpty
+		return err
+	}
+	return nil
+}
+
+func validateEmptyField(request *job.CreateSingleJobRequest) []string {
+	var emptyFields []string
+	if request.CommonJobInfo.SchedulingPolicy.QueueID == "" {
+		emptyFields = append(emptyFields, "queue")
+	}
+	if request.Image == "" {
+		emptyFields = append(emptyFields, "image")
+	}
+	if request.Flavour.Name == "" {
+		emptyFields = append(emptyFields, "flavour.name")
+	}
+	if request.FileSystem.Name == "" {
+		emptyFields = append(emptyFields, "fileSystem.name")
+	}
+	return emptyFields
+}
+
+func validateDistributedJob(ctx *logger.RequestContext, request *job.CreateDisJobRequest) error {
+	// todo(zhongzichao)
+	return nil
+}
+
+func validateWorkflowJob(ctx *logger.RequestContext, request *job.CreateWfJobRequest) error {
+	// todo(zhongzichao)
+	return nil
+}
---
+++ pkg/apiserver/router/v1/job_test.go
@@ -0,0 +1,154 @@
+/*
+Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"paddleflow/pkg/apiserver/controller/job"
+	"paddleflow/pkg/apiserver/models"
+	"paddleflow/pkg/common/config"
+	"paddleflow/pkg/common/logger"
+	"paddleflow/pkg/common/schema"
+)
+
+const (
+	MockQueueID   = "mockQueueID"
+	MockQueueName = "mockQueueName"
+)
+
+var (
+	queue1 = models.Queue{
+		Model: models.Model{
+			ID: MockQueueID,
+		},
+		Name:      MockQueueName,
+		Namespace: "paddleflow",
+		ClusterId: MockClusterID,
+		QuotaType: schema.TypeVolcanoCapabilityQuota,
+		MaxResources: schema.ResourceInfo{
+			CPU: "10",
+			Mem: "1000",
+			ScalarResources: schema.ScalarResourcesType{
+				"nvidia.com/gpu": "500",
+			},
+		},
+		SchedulingPolicy: []string{"s1", "s2"},
+		Status:           schema.StatusQueueOpen,
+	}
+)
+
+type args struct {
+	ctx *logger.RequestContext
+	req *job.CreateSingleJobRequest
+}
+
+func initQueue(t *testing.T, userName string) {
+	ctx := &logger.RequestContext{UserName: userName}
+	err := models.CreateQueue(ctx, &queue1)
+	assert.Nil(t, err)
+}
+
+func TestCreateJob(t *testing.T) {
+	router, baseURL := prepareDBAndAPIForUser(t, MockRootUser)
+	initCluster(t)
+	initQueue(t, mockUserName)
+
+	flavourName := initFlavour(t)
+	config.GlobalServerConfig.FlavourMap = map[string]schema.Flavour{
+		flavourName: {
+			Name: flavourName,
+			ResourceInfo: schema.ResourceInfo{
+				CPU: "1",
+				Mem: "100M",
+			},
+		},
+		"cpu": {
+			Name: "cpu",
+			ResourceInfo: schema.ResourceInfo{
+				CPU: "1",
+				Mem: "100M",
+			},
+		},
+		"gpu": {
+			Name: "gpu",
+			ResourceInfo: schema.ResourceInfo{
+				CPU: "1",
+				Mem: "100M",
+				ScalarResources: schema.ScalarResourcesType{
+					"nvidia.com/gpu": "500M",
+				},
+			},
+		},
+	}
+	ctx := &logger.RequestContext{UserName: "testusername"}
+	tests := []struct {
+		name         string
+		args         args
+		wantErr      bool
+		responseCode int
+	}{
+		{
+			name: "empty",
+			args: args{
+				ctx: ctx,
+				req: &job.CreateSingleJobRequest{},
+			},
+			wantErr:      false,
+			responseCode: 400,
+		},
+		{
+			name: "normal",
+			args: args{
+				ctx: ctx,
+				req: &job.CreateSingleJobRequest{
+					CommonJobInfo: job.CommonJobInfo{
+						Name:        "normal",
+						Labels:      map[string]string{},
+						Annotations: map[string]string{},
+						SchedulingPolicy: job.SchedulingPolicy{
+							QueueID: MockQueueID,
+						},
+					},
+					Image: "mockImage",
+					Flavour: schema.Flavour{
+						Name: flavourName,
+					},
+					FileSystem: schema.FileSystem{
+						Name: MockFsName1,
+					},
+				},
+			},
+			wantErr:      false,
+			responseCode: 200,
+		},
+	}
+	for _, tt := range tests {
+		t.Logf("baseURL=%s", baseURL)
+		res, err := PerformPostRequest(router, baseURL+"/job/single", tt.args.req)
+		t.Logf("create single job, response=%+v", res)
+		if tt.wantErr {
+			assert.Error(t, err)
+			continue
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, tt.responseCode, res.Code)
+		}
+	}
+}
