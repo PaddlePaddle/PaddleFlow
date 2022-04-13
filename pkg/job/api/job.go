502115763222d744896ee5130904fa99bdf11140
zhongzichao <zhongzichao@baidu.com>
2022-04-13 20:52:06 +0800

    add job executor

--- pkg/job/api/job.go
+++ pkg/job/api/job.go
@@ -137,6 +137,9 @@ func NewJobInfo(job *models.Job) (*PFJob, error) {
 		Tasks:     job.Members,
 	}
 	if len(job.ExtensionTemplate) == 0 {
+		if job.Config.GetFS() == "" || job.Config.GetYamlPath() == "" {
+			return pfjob, nil
+		}
 		var err error
 		pfjob.ExtRuntimeConf, err = pfjob.GetExtRuntimeConf(job.Config.GetFS(), job.Config.GetYamlPath())
 		if err != nil {
