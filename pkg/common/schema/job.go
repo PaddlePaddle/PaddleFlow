b55dd79918a6ef2e5dac31f00f2501f2c5c0fe0f
dongzezhao <dongzezhao@baidu.com>
2022-04-18 10:42:13 +0800

    Sync node status for ArgoWorkflow (#108)
    * add node status sync for ArgoWorkflow
    
    * update job id

--- pkg/common/schema/job.go
+++ pkg/common/schema/job.go
@@ -102,11 +102,12 @@ const (
 	TypeDistributed JobType = "distributed"
 	TypeWorkflow    JobType = "workflow"
 
-	FrameworkSpark   Framework = "spark"
-	FrameworkMPI     Framework = "mpi"
-	FrameworkTF      Framework = "tensorflow"
-	FrameworkPytorch Framework = "pytorch"
-	FrameworkPaddle  Framework = "paddle"
+	FrameworkSpark      Framework = "spark"
+	FrameworkMPI        Framework = "mpi"
+	FrameworkTF         Framework = "tensorflow"
+	FrameworkPytorch    Framework = "pytorch"
+	FrameworkPaddle     Framework = "paddle"
+	FrameworkStandalone Framework = "standalone"
 
 	// job priority
 	EnvJobVeryLowPriority  = "VERY_LOW"
