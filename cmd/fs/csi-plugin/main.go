b3492c0c86c7bb4d7f1124b7e1285987749822d0
elsie.fan <fanjiangxun@baidu.com>
2022-04-22 11:13:56 +0800

    fs csi-plugin init pod env

--- cmd/fs/csi-plugin/main.go
+++ cmd/fs/csi-plugin/main.go
@@ -19,6 +19,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"paddleflow/pkg/fs/csiplugin/client/k8s"
+	"paddleflow/pkg/fs/csiplugin/csiconfig"
 	"time"
 
 	"github.com/gin-contrib/pprof"
@@ -43,38 +45,38 @@ var logConf = logger.LogConfig{
 	IsCompress:      true,
 }
 
-//// init() obtain csi-plugin pod, to assign same parameters to mount pods in csiconfig
-//func init() {
-//	csiconfig.Namespace = os.Getenv("CSI_NAMESPACE")
-//	csiconfig.NodeName = os.Getenv("KUBE_NODE_NAME")
-//	csiconfig.PodName = os.Getenv("CSI_POD_NAME")
-//
-//	if csiconfig.PodName == "" || csiconfig.Namespace == "" {
-//		log.Fatalf("Pod name[%s] & namespace[%s] can't be null\n", csiconfig.PodName, csiconfig.Namespace)
-//		os.Exit(0)
-//	}
-//
-//	k8sClient, err := k8s.GetK8sClient()
-//	if err != nil {
-//		log.Errorf("get k8s client failed: %v", err)
-//		return
-//	}
-//	pod, err := k8sClient.GetPod(csiconfig.PodName, csiconfig.Namespace)
-//	if err != nil {
-//		log.Infof("Can't get pod %s: %v", csiconfig.PodName, err)
-//		os.Exit(0)
-//	}
-//	csiconfig.CSIPod = *pod
-//	for i := range pod.Spec.Containers {
-//		if pod.Spec.Containers[i].Name == "csi-storage-driver" {
-//			csiconfig.MountImage = pod.Spec.Containers[i].Image
-//			//csiconfig.ContainerResource = pod.Spec.Containers[i].Resources
-//			return
-//		}
-//	}
-//	log.Infof("Can't get container csi-storage-driver in pod %s", csiconfig.PodName)
-//	os.Exit(0)
-//}
+// init() obtain csi-plugin pod, to assign same parameters to mount pods in csiconfig
+func init() {
+	csiconfig.Namespace = os.Getenv("CSI_NAMESPACE")
+	csiconfig.NodeName = os.Getenv("KUBE_NODE_NAME")
+	csiconfig.PodName = os.Getenv("CSI_POD_NAME")
+
+	if csiconfig.PodName == "" || csiconfig.Namespace == "" {
+		log.Fatalf("Pod name[%s] & namespace[%s] can't be null\n", csiconfig.PodName, csiconfig.Namespace)
+		os.Exit(0)
+	}
+
+	k8sClient, err := k8s.GetK8sClient()
+	if err != nil {
+		log.Errorf("get k8s client failed: %v", err)
+		return
+	}
+	pod, err := k8sClient.GetPod(csiconfig.PodName, csiconfig.Namespace)
+	if err != nil {
+		log.Infof("Can't get pod %s: %v", csiconfig.PodName, err)
+		os.Exit(0)
+	}
+	csiconfig.CSIPod = *pod
+	for i := range pod.Spec.Containers {
+		if pod.Spec.Containers[i].Name == "csi-storage-driver" {
+			csiconfig.MountImage = pod.Spec.Containers[i].Image
+			//csiconfig.ContainerResource = pod.Spec.Containers[i].Resources
+			return
+		}
+	}
+	log.Infof("Can't get container csi-storage-driver in pod %s", csiconfig.PodName)
+	os.Exit(0)
+}
 
 func main() {
 	cli.VersionFlag = &cli.BoolFlag{
