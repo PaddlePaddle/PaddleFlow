287bf2a77c47e7016a1671348a585c80ba3ab690
dongzezhao <dongzezhao@baidu.com>
2022-04-25 16:20:14 +0800

    fix that mysql filed default is case insensitive

--- installer/database/paddleflow.sql
+++ installer/database/paddleflow.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `cluster_info` (
     PRIMARY KEY (`pk`),
     UNIQUE KEY idx_name (`name`, `deleted_at`),
     UNIQUE KEY idx_id (`id`, `deleted_at`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 CREATE TABLE IF NOT EXISTS `flavour` (
     `pk` bigint(20) NOT NULL AUTO_INCREMENT,
