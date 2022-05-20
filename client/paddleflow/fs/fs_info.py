cb2d537dd9602b357c409d86a17cb8ba56939d0a
fanjiangxun <fanjiangxun@baidu.com>
2022-05-20 10:37:23 +0800

    python cli: fs cache config (#241)

--- client/paddleflow/fs/fs_info.py
+++ client/paddleflow/fs/fs_info.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
+Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@ limitations under the License.
 
 #!/usr/bin/env python3
 # -*- coding:utf8 -*-
+
 
 class FSInfo(object):
     """the class of fs info"""
@@ -39,3 +40,13 @@ class LinkInfo(object):
         self.server_adddress = server_address
         self.subpath = subpath
         self.properties = properties
+
+
+class FsCacheConfig(object):
+    """the class of fs cache config"""
+    def __init__(self, fsid, cachepath, cachedriver, blocksize):
+        """init """
+        self.fsid = fsid
+        self.cachepath = cachepath
+        self.cachedriver = cachedriver
+        self.blocksize = blocksize
