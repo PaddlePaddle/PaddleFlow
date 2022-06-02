e866c95b0c1c6779287e52229f77f994a0efe8bf
luoyuedong <luoyuedong@baidu.com>
2022-06-02 20:46:15 +0800

    fix: arm64 adaptation

--- pkg/fs/client/ufs/attr_linux_uint32.go
+++ pkg/fs/client/ufs/attr_linux_uint32.go
@@ -1,15 +1,12 @@
 // +build linux
-// +build amd64 ppc64
+// +build 386 arm arm64
 
 /*
-Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
-
+Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,12 +20,12 @@ import "syscall"
 
 func fillStat(nlink uint64, mode uint32, uid uint32, gid uint32, size int64, blksize int64, blocks int64, atime, mtime, ctime syscall.Timespec) syscall.Stat_t {
 	return syscall.Stat_t{
-		Nlink:   nlink,
+		Nlink:   uint32(nlink),
 		Mode:    mode,
 		Uid:     uid,
 		Gid:     gid,
 		Size:    size,
-		Blksize: blksize,
+		Blksize: int32(blksize),
 		Blocks:  blocks,
 		Atim:    atime,
 		Mtim:    mtime,
