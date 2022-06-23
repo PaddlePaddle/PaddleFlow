175f7d4bc4ffae1e2f29a18f21779471274851a1
feidongdong <luoyuedong@baidu.com>
2022-06-23 16:35:49 +0800

    feature: add glusterfs (#381)
    * feature: add glusterfs
    
    * feature: add glusterfs
    
    * feature: add glusterfs

--- pkg/fs/common/fs_meta.go
+++ pkg/fs/common/fs_meta.go
@@ -26,6 +26,7 @@ const (
 	SFTPType             = "sftp"
 	MockType             = "mock"
 	CFSType              = "cfs"
+	GlusterfsType        = "glusterfs"
 
 	// common
 	Owner = "owner"
@@ -34,6 +35,7 @@ const (
 	// local properties and root path
 	RootKey = "root"
 	SubPath = "subpath"
+	Type    = "type"
 
 	// HDFS properties
 	NameNodeAddress = "dfs.namenode.address"
