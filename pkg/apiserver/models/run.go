305ad1b7862a27645371875ca91f664b514cf6f0
ZeroOneCode <caoyang1015@outlook.com>
2022-06-28 18:56:53 +0800

    test

--- pkg/apiserver/models/run.go
+++ pkg/apiserver/models/run.go
@@ -237,16 +237,17 @@ func (r *Run) initRuntime(jobs []RunJob, dags []RunDag) error {
 
 // 补全ComponentView中的Deps
 func ProcessRuntimeView(componentViews map[string][]schema.ComponentView, components map[string]schema.Component) {
-	logger.Logger().Info("debug: inin ProcessRuntimeView")
+	logger.Logger().Infof("debug: inin ProcessRuntimeView")
 	for compName, comp := range components {
-		logger.Logger().Info("debug: compName is: %s", compName)
+		logger.Logger().Infof("debug: compName is: %s", compName)
 		compViewList := componentViews[compName]
 		deps := strings.Join(comp.GetDeps(), ",")
-		logger.Logger().Info("debug: deps is: %s", comp.GetDeps())
-		logger.Logger().Info("debug: deps is: %s", deps)
+		logger.Logger().Infof("debug: deps is: %s", comp.GetDeps())
+		logger.Logger().Infof("debug: deps is: %s", deps)
 		for i, compView := range compViewList {
 			// 信息补全
 			compView.SetDeps(deps)
+			logger.Logger().Infof("debug: after set deps is: %s", compView.GetDeps())
 			logger.Logger().Infof("debug: after set deps is: %s", compViewList[i].GetDeps())
 			if dagView, ok := compView.(*schema.DagView); ok {
 				dag := comp.(*schema.WorkflowSourceDag)
