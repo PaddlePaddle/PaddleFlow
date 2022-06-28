f1339277ecc212ee5ebd177625d5e01c27bcd404
fanjiangxun <fanjiangxun@baidu.com>
2022-06-28 19:19:16 +0800

    csi => pfs-fuse deliver fs info - ut

--- pkg/fs/csiplugin/mount/info_test.go
+++ pkg/fs/csiplugin/mount/info_test.go
@@ -26,15 +26,15 @@ func TestKubeRuntimePVAndPVC(t *testing.T) {
 		UserName:      "root",
 		Name:          "testfs",
 		Type:          "s3",
-		SubPath:       "/elsies3",
-		ServerAddress: "s3.bj.bcebos.com",
+		SubPath:       "/supath",
+		ServerAddress: "server_address",
 
 		PropertiesMap: map[string]string{
-			"accessKey": "529dc59c70314dbeabf86d3972c66238",
-			"bucket":    "easydata-test",
-			"endpoint":  "s3.bj.bcebos.com",
+			"accessKey": "accessKey",
+			"bucket":    "bucket",
+			"endpoint":  "server_address",
 			"region":    "bj",
-			"secretKey": "a93c7ea50056869d72960a6a5be74084df3f70f25d9f7fa2154e5625d6c63a04f6f0a15ed34a4afac90a3cbc300c9d91"},
+			"secretKey": "secretKey"},
 	}
 	err := storage.Filesystem.CreatFileSystem(&fs)
 	assert.Nil(t, err)
@@ -62,7 +62,7 @@ func TestKubeRuntimePVAndPVC(t *testing.T) {
 
 	mountInfo, err := ProcessMountInfo(fs.ID, "server", fsBase64, fsCacheBase64, false)
 	assert.Nil(t, err)
-	assert.Equal(t, mountInfo.FsBase64Str, string(fsStr))
+	assert.Equal(t, mountInfo.FsBase64Str, fsBase64)
 	assert.Equal(t, mountInfo.FsCacheConfig.CacheDir, fsCache.CacheDir)
 	assert.Equal(t, mountInfo.FsCacheConfig.FsID, fsCache.FsID)
 	assert.Equal(t, mountInfo.FsCacheConfig.MetaDriver, fsCache.MetaDriver)
