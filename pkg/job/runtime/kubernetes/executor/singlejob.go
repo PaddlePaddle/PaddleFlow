e21bb437a647fea01e7d9ff484889160994dda47
zhongzichao <zhongzichao@baidu.com>
2022-06-28 16:29:29 +0800

    template and task choose one

--- pkg/job/runtime/kubernetes/executor/singlejob.go
+++ pkg/job/runtime/kubernetes/executor/singlejob.go
@@ -157,7 +157,7 @@ func (sp *SingleJob) fillContainer(container *v1.Container, podName string) erro
 		container.Image = sp.Image
 	}
 	// fill command
-	if len(container.Command) == 0 && sp.IsCustomYaml || !sp.IsCustomYaml {
+	if !sp.IsCustomYaml {
 		if sp.Command == "" {
 			return errors.EmptyJobCommandError()
 		}
