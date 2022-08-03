963bffb016071455ce8881869e02b836ac7ce35c
ZeroOneCode <caoyang1015@outlook.com>
2022-08-03 10:47:15 +0800

    test

--- pkg/apiserver/common/status.go
+++ pkg/apiserver/common/status.go
@@ -28,9 +28,6 @@ const (
 	StatusRunTerminated  = "terminated"
 	StatusRunSkipped     = "skipped"
 
-	StatusFinal  = "final"
-	StatusActive = "active"
-
 	WfEventKeyRunID         = "runID"
 	WfEventKeyPK            = "pk"
 	WfEventKeyStatus        = "status"
