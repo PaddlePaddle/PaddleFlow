3853bf8a9533a9c7579a274a7e3269ccaa80485b
luoyuedong <luoyuedong@baidu.com>
2022-08-09 14:55:41 +0800

    fix: kernerl cache expire fix

--- pkg/fs/client/fuse/pfs.go
+++ pkg/fs/client/fuse/pfs.go
@@ -33,8 +33,8 @@ type FuseConfig struct {
 	Uid          int
 	Gid          int
 	RawOwner     bool
-	EntryTimeout int
-	AttrTimeout  int
+	EntryTimeout time.Duration
+	AttrTimeout  time.Duration
 	DirMode      int
 	FileMode     int
 }
@@ -95,7 +95,7 @@ func (fs *PFS) GetAttr(cancel <-chan struct{}, input *fuse.GetAttrIn, out *fuse.
 		return fuse.Status(code)
 	}
 	attrToStat(entry.Ino, entry.Attr, &out.Attr)
-	out.AttrValid = uint64(FuseConf.AttrTimeout)
+	out.AttrValid = uint64(FuseConf.AttrTimeout.Seconds())
 	if vfs.IsSpecialNode(meta.Ino(input.NodeId)) {
 		out.AttrValid = 3600
 	}
@@ -422,12 +422,12 @@ func (fs *PFS) replyEntry(entry *meta.Entry, out *fuse.EntryOut) {
 	out.NodeId = uint64(entry.Ino)
 	// todo:: Generation这个配置是干啥的，得在看看
 	out.Generation = 1
-	out.SetAttrTimeout(time.Duration(FuseConf.AttrTimeout))
+	out.SetAttrTimeout(FuseConf.AttrTimeout)
 	if entry.Attr.Type == meta.TypeDirectory {
 		// todo:: 增加dirEntry配置，目录和目录项超时分开设置
-		out.SetEntryTimeout(time.Duration(FuseConf.EntryTimeout))
+		out.SetEntryTimeout(FuseConf.EntryTimeout)
 	} else {
-		out.SetEntryTimeout(time.Duration(FuseConf.EntryTimeout))
+		out.SetEntryTimeout(FuseConf.EntryTimeout)
 	}
 	if vfs.IsSpecialNode(entry.Ino) {
 		out.SetAttrTimeout(time.Hour)
