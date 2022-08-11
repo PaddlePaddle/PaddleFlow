bbb84916bd2c45f4f0e69be78988ff7215b6ae93
Alex Wang <wangxinmeng@baidu.com>
2022-08-11 21:24:56 +0800

    feat/add tf job support(#589)
    * feat(executor): add tf job support
    
    * test(executor): add tf job test.
    
    * feat(executor): add debug info

---
+++ pkg/job/runtime/kubernetes/executor/tensorflow_job.go
@@ -0,0 +1,124 @@
+/*
+Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package executor
+
+import (
+	"fmt"
+
+	tfv1 "github.com/kubeflow/training-operator/pkg/apis/tensorflow/v1"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/PaddlePaddle/PaddleFlow/pkg/common/k8s"
+	"github.com/PaddlePaddle/PaddleFlow/pkg/common/resources"
+	"github.com/PaddlePaddle/PaddleFlow/pkg/common/schema"
+)
+
+type TFJob struct {
+	KubeJob
+}
+
+func (j *TFJob) CreateJob() (string, error) {
+	log.Debugf("tf job %s creating, job struct: %+v", j.Name, j)
+	tfJob := &tfv1.TFJob{}
+	if err := j.createJobFromYaml(tfJob); err != nil {
+		log.Errorf("create %s failed, err %v", j.String(), err)
+		return "", err
+	}
+	if err := j.validateJob(); err != nil {
+		log.Errorf("validate %s failed, err %v", j.String(), err)
+		return "", err
+	}
+
+	var err error
+	// set metadata field
+	j.patchMetadata(&tfJob.ObjectMeta, j.ID)
+	// set spec field
+	if j.IsCustomYaml {
+		// set custom TFJob Spec from user
+		err = j.customTFJobSpec(&tfJob.Spec)
+	} else {
+		// set builtin TFJob Spec
+		err = j.builtinTFJobSpec(&tfJob.Spec)
+	}
+	if err != nil {
+		log.Errorf("build %s spec failed, err %v", j.String(), err)
+		return "", err
+	}
+	// create job on cluster
+	log.Infof("create %s on cluster", j.String())
+	log.Debugf("tf job %s created, job struct: %+v", j.Name, j)
+	if err = Create(tfJob, j.GroupVersionKind, j.DynamicClientOption); err != nil {
+		log.Errorf("create %s on cluster failed, err: %v", j.String(), err)
+		return "", err
+	}
+	return j.ID, err
+}
+
+// builtinTensorFlowJobSpec set build-in TFJob spec
+// TODO: add GPU supports for TFJob, now only support for CPU type job
+func (j *TFJob) builtinTFJobSpec(tfJobSpec *tfv1.TFJobSpec) error {
+	log.Debugf("patch %s spec:%#v", j.String(), tfJobSpec)
+	// TODO: set ElasticPolicy for TFJob
+	// set TFReplicaSpecs
+	minResources := resources.EmptyResource()
+	for _, task := range j.Tasks {
+		// tf parameter server for distributed training
+		replicaType := tfv1.TFReplicaTypePS
+		// if role is worker, set it to tf worker
+		if task.Role == schema.RoleWorker || task.Role == schema.RolePWorker {
+			// tf worker for distributed training
+			replicaType = tfv1.TFReplicaTypeWorker
+		}
+		replicaSpec, ok := tfJobSpec.TFReplicaSpecs[replicaType]
+		if !ok {
+			return fmt.Errorf("replica type %s for %s is not supported", replicaType, j.String())
+		}
+		if err := j.kubeflowReplicaSpec(replicaSpec, &task); err != nil {
+			log.Errorf("build %s RepilcaSpec for %s failed, err: %v", replicaType, j.String(), err)
+			return err
+		}
+		// calculate job minResources
+		taskResources, err := resources.NewResourceFromMap(task.Flavour.ToMap())
+		if err != nil {
+			log.Errorf("parse resources for %s task failed, err: %v", j.String(), err)
+			return err
+		}
+		taskResources.Multi(task.Replicas)
+		minResources.Add(taskResources)
+	}
+	// set RunPolicy
+	resourceList := k8s.NewResourceList(minResources)
+	return j.kubeflowRunPolicy(&tfJobSpec.RunPolicy, &resourceList)
+}
+
+func (j *TFJob) validateCustomYaml(tfSpec *tfv1.TFJobSpec) error {
+	log.Infof("validate custom yaml for %s, pdj from yaml: %v", j.String(), tfSpec)
+	// TODO: add validate for custom yaml
+	return nil
+}
+
+// customPyTorchJobSpec set custom PyTorchJob Spec
+func (j *TFJob) customTFJobSpec(tfJobSpec *tfv1.TFJobSpec) error {
+	log.Debugf("patch %s spec:%#v", j.String(), tfJobSpec)
+	err := j.validateCustomYaml(tfJobSpec)
+	if err != nil {
+		return err
+	}
+	// TODO: patch tf job from user
+	// check RunPolicy
+	return nil
+}
---
+++ pkg/job/runtime/kubernetes/executor/tensorflow_job_test.go
@@ -0,0 +1,123 @@
+/*
+Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package executor
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	tfv1 "github.com/kubeflow/training-operator/pkg/apis/tensorflow/v1"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/PaddlePaddle/PaddleFlow/pkg/common/k8s"
+	"github.com/PaddlePaddle/PaddleFlow/pkg/common/schema"
+	"github.com/PaddlePaddle/PaddleFlow/pkg/job/api"
+	"github.com/PaddlePaddle/PaddleFlow/pkg/model"
+	"github.com/PaddlePaddle/PaddleFlow/pkg/storage/driver"
+)
+
+func TestTFJob_CreateJob(t *testing.T) {
+	initGlobalServerConfig()
+	var server = httptest.NewServer(k8s.DiscoveryHandlerFunc)
+	defer server.Close()
+	dynamicClient := newFakeDynamicClient(server)
+	// mock db
+	driver.InitMockDB()
+	// create kubernetes resource with dynamic client
+	tests := []struct {
+		caseName  string
+		jobObj    *api.PFJob
+		expectErr error
+		wantErr   bool
+		wantMsg   string
+	}{
+		{
+			caseName: "tf_test1",
+			jobObj: &api.PFJob{
+				Name:      "test-tf-job",
+				ID:        "job-test-tf",
+				Namespace: "default",
+				JobType:   schema.TypeDistributed,
+				Framework: schema.FrameworkTF,
+				Conf: schema.Conf{
+					Name:    "normal",
+					Command: "sleep 200",
+					Image:   "mockImage",
+					Env: map[string]string{
+						"PF_JOB_MODE": "Collective",
+					},
+				},
+				Tasks: []model.Member{
+					{
+						Replicas: 1,
+						Role:     schema.RoleMaster,
+						Conf: schema.Conf{
+							Flavour: schema.Flavour{Name: "", ResourceInfo: schema.ResourceInfo{CPU: "4", Mem: "4Gi"}},
+						},
+					},
+					{
+						Replicas: 2,
+						Role:     schema.RoleWorker,
+						Conf: schema.Conf{
+							Flavour: schema.Flavour{Name: "", ResourceInfo: schema.ResourceInfo{CPU: "4", Mem: "4Gi"}},
+						},
+					},
+				},
+			},
+			expectErr: nil,
+			wantErr:   false,
+		},
+	}
+
+	for _, test := range tests {
+		p, err := NewKubeJob(test.jobObj, dynamicClient)
+		if test.wantErr {
+			assert.NotNil(t, err)
+			t.Logf("%s: create kube job failed, err: %v", test.caseName, err)
+			continue
+		}
+		assert.NotNil(t, p)
+		t.Logf("case[%s] to NewKubeJob=%+v", test.caseName, p)
+
+		jobID, err := p.CreateJob()
+		if err != nil {
+			t.Logf("create job failed, err: %v", err)
+		}
+		assert.Equal(t, test.expectErr, err)
+		if assert.NotEmpty(t, jobID) {
+			t.Logf("case[%s] to CreateJob, paddleFlowJob=%+v", test.caseName, p)
+			jobObj, err := Get(test.jobObj.Namespace, test.jobObj.ID, k8s.TFJobGVK, dynamicClient)
+			if !assert.NoError(t, err) {
+				t.Errorf(err.Error())
+				continue
+			}
+			t.Logf("obj=%#v", jobObj)
+			tfJob := tfv1.TFJob{}
+			err = runtime.DefaultUnstructuredConverter.FromUnstructured(jobObj.Object, &tfJob)
+			assert.NoError(t, err)
+			t.Logf("torch job %#v", tfJob)
+			podYaml, err := yaml.Marshal(tfJob)
+			assert.NoError(t, err)
+			t.Logf("%s", string(podYaml))
+			t.Skipped()
+		} else {
+			t.Errorf("error case, %v", err)
+		}
+	}
+}
