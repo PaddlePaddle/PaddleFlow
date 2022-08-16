96d734ed4e0ec902548ea789fd9ed059e3009a0c
zhongzichao <zhongzichao@baidu.com>
2022-08-16 15:11:14 +0800

    add RBAC for training job

--- pkg/job/runtime/kubernetes/executor/kube_builtin_job.go
+++ pkg/job/runtime/kubernetes/executor/kube_builtin_job.go
@@ -108,7 +108,9 @@ func (j *KubeJob) setPodContainer(container *corev1.Container, task *model.Membe
 	container.Env = j.appendEnvIfAbsent(container.Env, j.generateEnvVars())
 	// set container Command and Args
 	workDir := j.getWorkDir(task)
-	container.Command = j.generateContainerCommand(task.Command, workDir)
+	if task.Command != "" {
+		container.Command = j.generateContainerCommand(task.Command, workDir)
+	}
 	if len(task.Args) > 0 {
 		container.Args = task.Args
 	}
