2ce04386b8f3eb6b4682cb6ec2fb5c3e16007492
zhongzichao <zhongzichao@baidu.com>
2022-08-22 20:25:58 +0800

    add job and queue checks

--- uninstaller/pre-delete.py
+++ uninstaller/pre-delete.py
@@ -32,40 +32,125 @@ import os
 
 
 def check_pfserver_status(service_name, namespace, port, user, password):
-    host = "%s.%s.svc.cluster.local" % (service_name, namespace)
-    print(host)
-    
+    print("check_pfserver_status service_name=[%s] namespace=[%s] port=[%s] user=[%s] password=[%s]" % (
+    service_name, namespace, port, user, password))
+    host = service_name
+
     client = paddleflow.Client(host, user, password, port)
     ret, response = client.login(user, password)
     if not ret:
         print(response)
-        err_msg = "client login failed, with host[%s], user[%s], password[%s], port[%s], response[%s]" % (host, user, password, port, response)
+        err_msg = "client login failed, with host[%s], user[%s], password[%s], port[%s], response[%s]" % (
+        host, user, password, port, response)
         raise Exception(err_msg)
-    
+    sum = 1
+    while sum != 0:
+        print("\nscheduling clean paddleflow resource, current resource is %d" % sum)
+        sum = clean_pipelines(client)
+        jobs_nums = clean_jobs(client)
+        if jobs_nums == 0:
+            sum += clean_queue(client)
+        sum += jobs_nums
+        clean_storage(client)
+        if sum != 0:
+            time.sleep(5)
+
+
+def clean_pipelines(client):
+    print("clean_pipelines starting")
     active_status_list = ["initiating", "pending", "running", "terminating"]
-    while True:
-        ret, response = client.list_run(status=".".join(active_status_list))
-        if not ret:
-            print(response)
-            err_msg = "list run failed, with response[%s]" % (response)
-            raise Exception(err_msg)
-        
-        run_list = response["runList"]
-        if len(run_list) != 0:
-            err_msg = "there are [%s] active runs" % len(run_list)
-            print(err_msg)
-            time.sleep(5) 
-        else:
-            err_msg = "no active run, quit pre-delete check"
-            print(err_msg)
-            return 0
+    ret, response = client.list_run(status=".".join(active_status_list))
+    if not ret:
+        print(response)
+        err_msg = "list run failed, with response[%s]" % (response)
+        raise Exception(err_msg)
+
+    run_list = response["runList"]
+    if len(run_list) != 0:
+        err_msg = "there are [%s] active runs" % len(run_list)
+        print(err_msg)
+    else:
+        err_msg = "no active run, quit clean_pipelines check"
+        print(err_msg)
+    print("clean_pipelines end\n\n")
+    return len(run_list)
+
+
+def clean_storage(client):
+    print("clean_storage")
+    print("clean_storage end\n\n")
+    pass
+
+
+def clean_queue(client):
+    print("clean_queue")
+    ret, response, next_marker = client.list_queue()
+    if not ret:
+        print("clean queue failed, %s" % response)
+        err_msg = "list %s queue failed, with response[%s]" % (response)
+        raise Exception(err_msg)
+    queue_list = response
+    if len(queue_list) != 0:
+        for q in queue_list:
+            print(q.name, q.namespace)
+            delete_queue(client, q.name)
+
+        print("there are [%s] active queue" % len(queue_list))
+    else:
+        print("no active job, quit clean_queue check")
+    print("clean_queue finished\n\n")
+    return len(queue_list)
+
+
+def delete_queue(client, queue_name):
+    print("try to delete queue %s" % queue_name)
+    is_success, err_msg = client.del_queue(queue_name)
+    if not is_success:
+        print("failed to delete queue %s, err_msg= %s" % (queue_name, err_msg))
+    else:
+        print("delete queue %s successed." % queue_name)
+
+
+def clean_jobs(client):
+    print("clean_jobs")
+    job_status = ["pending", "running"]
+    jobs_nums = 0
+    for status in job_status:
+        next_marker = None
+        nums, next_marker = clean_jobs_with_status(client, status, next_marker=next_marker)
+        print("there are [%s] %s job" % (nums, status))
+        jobs_nums = jobs_nums + nums
+    return jobs_nums
+
+
+def clean_jobs_with_status(client, status, next_marker=None):
+    print("clean_jobs with [%s] status" % status)
+    ret, response, next_marker = client.list_job(status=status, marker=next_marker)
+    if not ret:
+        print(response)
+        err_msg = "list %s job failed, with response[%s]" % (status, response)
+        raise Exception(err_msg)
+    job_list = response
+    if len(job_list) != 0:
+        err_msg = "there are [%s] active jobs" % len(job_list)
+        print(err_msg)
+    else:
+        err_msg = "no [%s] job, quit clean_jobs_with_status check" % status
+        print(err_msg)
+    return len(job_list), next_marker
+
 
 if __name__ == '__main__':
+    print("pre-delete check starting")
     service_name = os.getenv("service_name")
     namespace = os.getenv("namespace")
     port = int(os.getenv("port"))
     user = os.getenv("user")
     password = os.getenv("password")
 
-    check_pfserver_status(service_name, namespace, port, user, password)
+    try:
+        check_pfserver_status(service_name, namespace, port, user, password)
+    except Exception as e:
+        print(e)
 
+    print("pre-delete check finished")
