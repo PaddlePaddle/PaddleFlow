596dde932522c20484a65feb7f71194917a5ae70
dongzezhao <dongzezhao@baidu.com>
2022-09-07 11:23:20 +0800

    Fix: builtin single job cannot read job template (#667)

--- pkg/job/runtime/kubernetes/executor/kubernetes_job.go
+++ pkg/job/runtime/kubernetes/executor/kubernetes_job.go
@@ -114,7 +114,7 @@ func NewKubeJob(job *api.PFJob, dynamicClientOpt *k8s.DynamicClientOption) (api.
 		Priority:            job.Conf.GetPriority(),
 		QueueName:           job.Conf.GetQueueName(),
 		DynamicClientOption: dynamicClientOpt,
-		YamlTemplateContent: []byte(job.ExtensionTemplate),
+		YamlTemplateContent: job.ExtensionTemplate,
 	}
 
 	switch job.JobType {
