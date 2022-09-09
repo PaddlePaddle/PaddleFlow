2fc9a531bbddbeaae40b4d25475c9c22b5cdc370
dongzezhao <dongzezhao@baidu.com>
2022-09-09 15:55:23 +0800

    fix job init bug

--- pkg/job/runtime_v2/kubernetes_runtime.go
+++ pkg/job/runtime_v2/kubernetes_runtime.go
@@ -44,6 +44,7 @@ import (
 	"github.com/PaddlePaddle/PaddleFlow/pkg/job/runtime_v2/client"
 	"github.com/PaddlePaddle/PaddleFlow/pkg/job/runtime_v2/controller"
 	"github.com/PaddlePaddle/PaddleFlow/pkg/job/runtime_v2/framework"
+	_ "github.com/PaddlePaddle/PaddleFlow/pkg/job/runtime_v2/jobs"
 	"github.com/PaddlePaddle/PaddleFlow/pkg/storage"
 )
 
