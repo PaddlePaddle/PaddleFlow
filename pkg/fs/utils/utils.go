9c1c082d1d25ae2b19f05bc6c5954a0a9a00e320
caizejun <caizejun@baidu.com>
2022-09-23 14:08:48 +0800

    bugfix: get fsName and username by fsId (#727)

--- pkg/fs/utils/utils.go
+++ pkg/fs/utils/utils.go
@@ -226,7 +226,7 @@ func FsIDToFsNameUsername(fsID string) (fsName, username string) {
 	return
 }
 
-func GetFsNameAndUserNameByFsID(fsID string) (userName, fsName string, err error) {
+func GetFsNameAndUserNameByFsID(fsID string) (fsName, username string, err error) {
 	fsArray := strings.Split(fsID, "-")
 	if len(fsArray) < 3 {
 		err = fmt.Errorf("fsID[%s] is not valid", fsID)
@@ -234,12 +234,12 @@ func GetFsNameAndUserNameByFsID(fsID string) (userName, fsName string, err error
 	}
 	if len(fsArray) > 3 {
 		// such as fs-root-v-xxxx
-		fsName = strings.Join(fsArray[2:len(fsArray)], "-")
-		userName = fsArray[1]
+		fsName = strings.Join(fsArray[2:], "-")
+		username = fsArray[1]
 		return
 	}
-	userName = strings.Join(fsArray[1:len(fsArray)-1], "")
-	fsName = fsArray[len(fsArray)-1]
+	username = fsArray[1]
+	fsName = fsArray[2]
 	return
 }
 
