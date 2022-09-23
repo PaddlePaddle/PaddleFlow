832eb82ecad0f8c65aa5f2ad008c2b094f921f35
loleek <lolee_k@163.com>
2022-09-23 15:37:31 +0800

    add rayjob in pipeline (#726)
    * add rayjob in pipeline
    
    * fix bug that ray without queue (#730)
    
    * fix ray operator create pod error
    
    Signed-off-by: zhongzichao <zhongzichao@baidu.com>

--- pkg/job/runtime/kubernetes/executor/ray_job.go
+++ pkg/job/runtime/kubernetes/executor/ray_job.go
@@ -28,12 +28,6 @@ import (
 	"github.com/PaddlePaddle/PaddleFlow/pkg/common/schema"
 )
 
-const (
-	envGroupName   = "GROUP_NAME"
-	envMinReplicas = "MIN_REPLICAS"
-	envMaxReplicas = "MAX_REPLICAS"
-)
-
 type RayJob struct {
 	KubeJob
 }
@@ -115,8 +109,8 @@ func fillRayJobSpec(rayJobSpec *rayV1alpha1.RayJobSpec, member schema.Member) {
 	// 	Entrypoint string `json:"entrypoint"`
 	rayJobSpec.Entrypoint = member.Command
 	//	RuntimeEnv string `json:"runtimeEnv,omitempty"`
-	if runtimeEnv, exist := member.Env["runtimeEnv"]; exist {
-		rayJobSpec.Entrypoint = runtimeEnv
+	if runtimeEnv, exist := member.Env[schema.EnvRayJobRuntimeEnv]; exist {
+		rayJobSpec.RuntimeEnv = runtimeEnv
 	}
 	//	ShutdownAfterJobFinishes bool `json:"shutdownAfterJobFinishes,omitempty"`
 	rayJobSpec.ShutdownAfterJobFinishes = true
@@ -148,6 +142,21 @@ func (j *RayJob) buildHeadPod(rayJobSpec *rayV1alpha1.RayJobSpec, member schema.
 		log.Errorf("failed to fill containers, err=%v", err)
 		return err
 	}
+	if preStop, exist := member.Env[schema.EnvRayJobHeaderPreStop]; exist {
+		for index, container := range headGroupSpec.Template.Spec.Containers {
+			if container.Name != "autoscaler" {
+				if container.Lifecycle == nil {
+					container.Lifecycle = &v1.Lifecycle{}
+				}
+				container.Lifecycle.PreStop = &v1.Handler{
+					Exec: &v1.ExecAction{
+						Command: []string{preStop},
+					},
+				}
+				headGroupSpec.Template.Spec.Containers[index] = container
+			}
+		}
+	}
 
 	return nil
 }
@@ -171,14 +180,14 @@ func (j *RayJob) buildWorkerPod(rayJobSpec *rayV1alpha1.RayJobSpec, member schem
 	}
 	//	todo ScaleStrategy defines which pods to remove
 	// GroupName
-	if groupName, exist := member.Env[envGroupName]; exist {
+	if groupName, exist := member.Env[schema.EnvRayJobWorkerGroupName]; exist {
 		worker.GroupName = groupName
 	}
 	// Replicas
 	replicas := int32(member.Replicas)
 	worker.Replicas = &replicas
 	// minReplicas
-	if val, exist, err := getInt32FromEnv(member.Env, envMinReplicas); err != nil {
+	if val, exist, err := getInt32FromEnv(member.Env, schema.EnvRayJobWorkerMinReplicas); err != nil {
 		err := fmt.Errorf("get minReplicas failed, err: %s", err)
 		log.Error(err)
 		return err
@@ -186,7 +195,7 @@ func (j *RayJob) buildWorkerPod(rayJobSpec *rayV1alpha1.RayJobSpec, member schem
 		worker.MinReplicas = &val
 	}
 	// maxReplicas
-	if val, exist, err := getInt32FromEnv(member.Env, envMaxReplicas); err != nil {
+	if val, exist, err := getInt32FromEnv(member.Env, schema.EnvRayJobWorkerMaxReplicas); err != nil {
 		err := fmt.Errorf("get maxReplicas failed, err: %s", err)
 		log.Error(err)
 		return err
