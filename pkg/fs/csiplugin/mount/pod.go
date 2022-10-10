0ca1be3a745af647c4ef2b669499737e1999bc96
fanjiangxun <fanjiangxun@baidu.com>
2022-10-10 17:05:15 +0800

    csi: fscache update

--- pkg/fs/csiplugin/mount/pod.go
+++ pkg/fs/csiplugin/mount/pod.go
@@ -194,7 +194,7 @@ func buildMountPod(volumeID string, mountInfo Info) (*k8sCore.Pod, error) {
 		csiconfig.NodeName, mountInfo.CacheConfig.CacheDir, mountInfo.CacheConfig.FsID)
 	pod.Labels[schema.LabelKeyNodeName] = csiconfig.NodeName
 	// cache dir has "/" and is not allowed in label
-	pod.Labels[schema.AnnotationKeyCacheDir] = mountInfo.CacheConfig.CacheDir
+	pod.Annotations[schema.AnnotationKeyCacheDir] = mountInfo.CacheConfig.CacheDir
 	return pod, nil
 }
 
