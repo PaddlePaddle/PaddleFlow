70503a1e6a8ccea5a77301338a7b3c473256631e
dongzezhao <dongzezhao@baidu.com>
2022-10-11 20:09:00 +0800

    fix ut

--- pkg/job/runtime_v2/job/argoworkflow/kube_workflow_job_test.go
+++ pkg/job/runtime_v2/job/argoworkflow/kube_workflow_job_test.go
@@ -80,7 +80,8 @@ spec:
 func TestWorkflowJob(t *testing.T) {
 	config.GlobalServerConfig = &config.ServerConfig{}
 	config.GlobalServerConfig.Job.SchedulerName = "testSchedulerName"
-	config.GlobalServerConfig.Job.DefaultJobYamlDir = "../../../../../config/server/default/job"
+	defaultJobYamlPath := "../../../../../config/server/default/job/job_template.yaml"
+	config.InitJobTemplate(defaultJobYamlPath)
 
 	var server = httptest.NewServer(k8s.DiscoveryHandlerFunc)
 	defer server.Close()
