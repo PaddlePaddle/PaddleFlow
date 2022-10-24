e23216642cc7e24abe3d0d6119a1ea258c61e7aa
fanjiangxun <fanjiangxun@baidu.com>
2022-10-24 18:29:04 +0800

    fs mount pod - resource limite & nodeAffinity - ut coverage

--- pkg/apiserver/controller/fs/cache_stats_test.go
+++ pkg/apiserver/controller/fs/cache_stats_test.go
@@ -39,6 +39,8 @@ const (
 	testTargetPath = "/var/lib/kubelet/pods/abc/volumes/kubernetes.io~csi/pfs-fs-root-test-default-pv/mount"
 	mockClusterID  = "cluster-mock"
 	mockFSID       = "fs-root-mock"
+	mockFSName     = "mock"
+	mockRootName   = "root"
 	mockNodename   = "nodename_mock"
 	mockCacheDir   = "/var/cache"
 )
