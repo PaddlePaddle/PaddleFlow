49ba88421e4b5d2a00e84a7496a0ed908fb6c2bd
fanjiangxun <fanjiangxun@baidu.com>
2022-10-25 16:56:56 +0800

    fs csi refactor

--- pkg/apiserver/controller/fs/fs.go
+++ pkg/apiserver/controller/fs/fs.go
@@ -125,7 +125,7 @@ func MountPodController(mountPodExpire, interval time.Duration,
 	for {
 		if times%3 == 0 {
 			times = 0
-			if err := cleanMountPod(mountPodExpire); err != nil {
+			if err := cleanExpiredMountPod(mountPodExpire); err != nil {
 				log.Errorf("clean mount pod err: %v", err)
 			}
 		}
