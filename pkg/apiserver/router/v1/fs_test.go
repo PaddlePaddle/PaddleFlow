e529aa86aeec304e8f0e561d197a981c0b2e5443
fanjiangxun <fanjiangxun@baidu.com>
2022-10-28 16:54:17 +0800

    fs check mounted and clean resources (#845)
    * fs check mounted and clean resources
    
    * fs check mounted and clean resources
    
    * fs check mounted and clean resources
    
    * fs check mounted and clean resources
    
    * fs check mounted and clean resources
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut
    
    * fs check mounted and clean resources - ut

--- pkg/apiserver/router/v1/fs_test.go
+++ pkg/apiserver/router/v1/fs_test.go
@@ -25,10 +25,12 @@ import (
 
 	"github.com/agiledragon/gomonkey/v2"
 	"github.com/stretchr/testify/assert"
+	k8sCore "k8s.io/api/core/v1"
 
 	"github.com/PaddlePaddle/PaddleFlow/pkg/apiserver/controller/fs"
 	"github.com/PaddlePaddle/PaddleFlow/pkg/common/logger"
 	fsCommon "github.com/PaddlePaddle/PaddleFlow/pkg/fs/common"
+	"github.com/PaddlePaddle/PaddleFlow/pkg/job/runtime"
 	"github.com/PaddlePaddle/PaddleFlow/pkg/model"
 	"github.com/PaddlePaddle/PaddleFlow/pkg/storage"
 )
@@ -475,10 +477,15 @@ func TestCreateFSAndDeleteFs(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, http.StatusCreated, result.Code)
 	// test delete fs successful
-	var p1 = gomonkey.ApplyMethod(reflect.TypeOf(fs.GetFileSystemService()), "CheckFsMountedAndCleanResources",
+	var p1 = gomonkey.ApplyPrivateMethod(reflect.TypeOf(fs.GetFileSystemService()), "checkFsMountedAllClustersAndScheduledJobs",
 		func(_ *fs.FileSystemService, fsID string) (bool, error) {
 			return false, nil
 		})
+	var p2 = gomonkey.ApplyPrivateMethod(reflect.TypeOf(fs.GetFileSystemService()), "cleanFsResources",
+		func(_ *fs.FileSystemService, runtimePodsMap map[*runtime.KubeRuntime][]k8sCore.Pod, fsID string) (err error) {
+			return nil
+		})
+	defer p2.Reset()
 	deleteUrl := fsUrl + "/" + mockFsName
 	result, err = PerformDeleteRequest(router, deleteUrl)
 	assert.Nil(t, err)
@@ -487,7 +494,7 @@ func TestCreateFSAndDeleteFs(t *testing.T) {
 	p1.Reset()
 
 	// test fs mounted
-	p1 = gomonkey.ApplyMethod(reflect.TypeOf(fs.GetFileSystemService()), "CheckFsMountedAndCleanResources",
+	p1 = gomonkey.ApplyPrivateMethod(reflect.TypeOf(fs.GetFileSystemService()), "checkFsMountedAllClustersAndScheduledJobs",
 		func(_ *fs.FileSystemService, fsID string) (bool, error) {
 			return true, nil
 		})
