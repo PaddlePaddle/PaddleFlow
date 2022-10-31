89f0790bf3e697ab68605863ddec5ed0a540581c
fanjiangxun <fanjiangxun@baidu.com>
2022-10-31 14:09:58 +0800

    fs cache stats - map enquery (#858)
    * fs cache stats - map enquery
    
    * fs cache stats - map enquery
    
    * fs cache stats - map enquery

--- pkg/apiserver/controller/fs/cache_stats.go
+++ pkg/apiserver/controller/fs/cache_stats.go
@@ -92,6 +92,11 @@ func updateMountPodCacheStats(clusterID string, k8sRuntime *runtime.KubeRuntime)
 }
 
 func syncCacheFromMountPod(pod *k8sCore.Pod, clusterID string) error {
+	if pod.Labels == nil || pod.Annotations == nil {
+		errRet := fmt.Errorf("mount pod[%s] Labels or Annotations is nil", pod.Name)
+		log.Errorf(errRet.Error())
+		return errRet
+	}
 	fsCache := &model.FSCache{ClusterID: clusterID}
 	for k, v := range pod.Labels {
 		switch k {
@@ -112,12 +117,13 @@ func syncCacheFromMountPod(pod *k8sCore.Pod, clusterID string) error {
 		}
 	}
 
-	for k, v := range pod.Annotations {
-		if k == schema.AnnotationKeyCacheDir {
-			fsCache.CacheDir = v
-			break
-		}
+	cacheDir, ok := pod.Annotations[schema.AnnotationKeyCacheDir]
+	if !ok {
+		errRet := fmt.Errorf("mount pod[%s] cache dir not exist in annotation", pod.Name)
+		log.Errorf(errRet.Error())
+		return errRet
 	}
+	fsCache.CacheDir = cacheDir
 
 	if fsCache.FsID == "" ||
 		fsCache.CacheID == "" ||
