6898746866e68050f2fed2d1fd0ac3d7d03561a3
StevenYangCao <caoyang1015@outlook.com>
2022-11-01 11:45:06 +0800

    temp

--- go-sdk/service/apiserver/v1/run.go
+++ go-sdk/service/apiserver/v1/run.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -116,6 +116,19 @@ type GetRunResponse struct {
 	CreateTime     string                 `json:"createTime"`
 	ActivateTime   string                 `json:"activateTime"`
 	UpdateTime     string                 `json:"updateTime,omitempty"`
+}
+
+func (r *GetRunResponse) GetStartTime() string {
+	return r.ActivateTime
+}
+
+func (r *GetRunResponse) GetEndTime() string {
+	for _, status := range common.RunFinalStatus {
+		if status == r.Status {
+			return r.UpdateTime
+		}
+	}
+	return ""
 }
 
 type StopRequest struct {
