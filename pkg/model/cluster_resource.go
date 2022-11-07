f078664ff3e89481000649057911cd97c050f580
dongzezhao <dongzezhao@baidu.com>
2022-11-07 19:20:18 +0800

    add index

--- pkg/model/cluster_resource.go
+++ pkg/model/cluster_resource.go
@@ -23,7 +23,7 @@ const (
 
 type NodeInfo struct {
 	Pk        int64  `gorm:"primaryKey;autoIncrement" json:"-"`
-	ID        string `gorm:"column:id" json:"nodeID"`
+	ID        string `gorm:"column:id;index:idx_id" json:"nodeID"`
 	Name      string `gorm:"column:name" json:"nodeName"`
 	ClusterID string `gorm:"column:cluster_id" json:"-"`
 	Status    string `gorm:"column:status" json:"nodeStatus"`
@@ -36,7 +36,7 @@ func (NodeInfo) TableName() string {
 
 type PodInfo struct {
 	Pk     int64  `gorm:"primaryKey;autoIncrement" json:"-"`
-	ID     string `gorm:"column:id" json:"podID"`
+	ID     string `gorm:"column:id;index:idx_id" json:"podID"`
 	Name   string `gorm:"column:name" json:"podName"`
 	NodeID string `gorm:"column:node_id" json:"nodeID"`
 	Status string `gorm:"column:status" json:"podStatus"`
@@ -48,10 +48,10 @@ func (PodInfo) TableName() string {
 
 type ResourceInfo struct {
 	Pk       int64  `gorm:"primaryKey;autoIncrement" json:"-"`
-	PodID    string `gorm:"column:pod_id" json:"podID"`
-	NodeID   string `gorm:"column:node_id" json:"nodeID"`
+	PodID    string `gorm:"column:pod_id;index:idx_pod_id" json:"podID"`
+	NodeID   string `gorm:"column:node_id;index:idx_node_id" json:"nodeID"`
 	NodeName string `gorm:"column:node_name" json:"nodeName"`
-	Name     string `gorm:"column:resource_name" json:"resourceName"`
+	Name     string `gorm:"column:resource_name;index:idx_resource_name" json:"resourceName"`
 	Value    int64  `gorm:"column:resource_value" json:"resourceValue"`
 	IsShared int    `gorm:"column:is_shared" json:"-"`
 }
@@ -63,9 +63,9 @@ func (ResourceInfo) TableName() string {
 type LabelInfo struct {
 	Pk         int64  `gorm:"primaryKey;autoIncrement" json:"-"`
 	ID         string `gorm:"column:id" json:"id"`
-	Name       string `gorm:"column:label_name" json:"labelName"`
+	Name       string `gorm:"column:label_name;index:idx_name" json:"labelName"`
 	Value      string `gorm:"column:label_value" json:"labelValue"`
-	ObjectID   string `gorm:"column:object_id" json:"objectID"`
+	ObjectID   string `gorm:"column:object_id;index:idx_obj_id" json:"objectID"`
 	ObjectType string `gorm:"column:object_type" json:"objectType"`
 }
 
