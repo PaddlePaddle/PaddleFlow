30b194b37bc906f81a1ad882a3de06265b5a1ee7
dongzezhao <dongzezhao@baidu.com>
2022-11-17 14:42:54 +0800

    remove useless code

--- pkg/apiserver/controller/cluster/cluster_resource.go
+++ pkg/apiserver/controller/cluster/cluster_resource.go
@@ -1,12 +1,10 @@
 package cluster
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
-	"k8s.io/apimachinery/pkg/api/resource"
 
 	"github.com/PaddlePaddle/PaddleFlow/pkg/apiserver/common"
 	"github.com/PaddlePaddle/PaddleFlow/pkg/common/k8s"
@@ -24,17 +22,6 @@ type ListClusterResourcesRequest struct {
 	PageSize        int      `json:"pageSize"`
 }
 
-type ListClusterResourceResponse struct {
-	Data []ClusterQuotaResponse `json:"data"`
-}
-
-type ClusterQuotaResponse struct {
-	Allocatable map[string]map[string]int64  `json:"allocatable"`
-	Capacity    map[string]map[string]string `json:"capacity"`
-	IsIsolation int                          `json:"isIsolation"`
-	ClusterName string                       `json:"clusterName"`
-}
-
 type NodeResourcesResponse struct {
 	Allocatable map[string]map[string]interface{} `json:"allocatable"`
 	Capacity    map[string]map[string]string      `json:"capacity"`
@@ -43,13 +30,12 @@ type NodeResourcesResponse struct {
 }
 
 // ListClusterResources return the node resources in clusters, lists can be filtered by labels in pods or nodes
-func ListClusterResources(ctx *logger.RequestContext, req ListClusterResourcesRequest) (interface{}, error) {
+func ListClusterResources(ctx *logger.RequestContext, req ListClusterResourcesRequest) (map[string]NodeResourcesResponse, error) {
 	log.Infof("list cluster resources request: %v", req)
-	var response ListClusterResourceResponse
 	if !common.IsRootUser(ctx.UserName) {
 		ctx.ErrorCode = common.OnlyRootAllowed
 		ctx.Logging().Errorln("list cluster resources failed. error: admin is needed.")
-		return response, errors.New("list cluster resources failed")
+		return nil, errors.New("list cluster resources failed")
 	}
 
 	// 1. list nodes
@@ -58,7 +44,7 @@ func ListClusterResources(ctx *logger.RequestContext, req ListClusterResourcesRe
 	if err != nil {
 		err = fmt.Errorf("list node from cache failed, err: %v", err.Error())
 		ctx.Logging().Errorln(err)
-		return response, err
+		return nil, err
 	}
 	ctx.Logging().Debugf("list nodes: %+v", nodes)
 	var nodeLists []string
@@ -70,7 +56,7 @@ func ListClusterResources(ctx *logger.RequestContext, req ListClusterResourcesRe
 	if err != nil {
 		err = fmt.Errorf("list node resources from cache failed, err: %v", err.Error())
 		ctx.Logging().Errorln(err)
-		return response, err
+		return nil, err
 	}
 	ctx.Logging().Debugf("list node resources: %v", result)
 	// 3. construct response
@@ -146,145 +132,4 @@ func getAllocatable(nodeCapacity map[string]string, usedResources map[string]int
 		capacity.Sub(used)
 		return capacity.ToMap(), nil
 	}
-}
-
-// ListClusterQuotaByLabels return the node resources in clusters, lists can be filtered by labels in pods or nodes Deprecated
-func ListClusterQuotaByLabels(ctx *logger.RequestContext, req ListClusterResourcesRequest) (ListClusterResourceResponse, error) {
-	log.Infof("clusterName list req: %v", req)
-	var response ListClusterResourceResponse
-	var dataList []ClusterQuotaResponse
-
-	if !common.IsRootUser(ctx.UserName) {
-		ctx.ErrorCode = common.OnlyRootAllowed
-		ctx.Logging().Errorln("get cluster quota failed. error: admin is needed.")
-		return response, errors.New("get cluster failed")
-	}
-
-	// 1. group by nodes to sum resource
-	var err error
-	var result []model.ResourceInfoResponse
-	if result, err = storage.ResourceCache.ListResouces(req.ClusterNameList, req.Labels, req.LabelType); err != nil {
-		err = fmt.Errorf("list resources from cache failed, err: %v", err.Error())
-		log.Errorln(err)
-		return response, err
-	}
-	// 2. build resources
-	clusterMap, err := buildClusterResponse(result)
-	if err != nil {
-		log.Errorln(err)
-		return response, err
-	}
-	if len(clusterMap) == 0 {
-		return ListClusterResourceResponse{Data: dataList}, nil
-	}
-	// 3. convert map to list
-	for _, v := range clusterMap {
-		dataList = append(dataList, v)
-	}
-	// 4. page limit
-	dataList, err = slicePages(dataList, req.PageSize, req.PageNo)
-	if err != nil {
-		log.Errorln(err)
-		return response, err
-	}
-	// calculate allocatable resource by capacity sub used
-	dataList, err = updateAllocatable(dataList)
-	if err != nil {
-		log.Errorf("get allocatable resource failed, err: %v", err)
-		return response, err
-	}
-
-	return ListClusterResourceResponse{Data: dataList}, nil
-}
-
-// buildClusterResponse convert sql results to map Deprecated
-func buildClusterResponse(result []model.ResourceInfoResponse) (map[string]ClusterQuotaResponse, error) {
-	log.Infof("cluster list queried from db, result: %v", result)
-	var err error
-	clusterMap := map[string]ClusterQuotaResponse{}
-	for _, resInfo := range result {
-		cName := resInfo.ClusterName
-		if _, exist := clusterMap[cName]; !exist {
-			clusterMap[cName] = ClusterQuotaResponse{
-				ClusterName: cName,
-				Allocatable: make(map[string]map[string]int64),
-				Capacity:    make(map[string]map[string]string),
-			}
-		}
-		cluster := clusterMap[cName]
-
-		// todo  capacity结构未最终确定,暂时使用map
-		if _, exist := cluster.Capacity[resInfo.NodeName]; !exist {
-			var capacity map[string]string
-			err = json.Unmarshal([]byte(resInfo.CapacityJSON), &capacity)
-			if err != nil {
-				log.Errorf("list cluster resources by label failed, unmarshal failed, err: %v", err)
-				return nil, err
-			}
-			cluster.Capacity[resInfo.NodeName] = capacity
-		}
-		// todo allocatable 需要用capacity去做减法, 由于结构未最终确定,暂时展示为已使用资源量
-		// todo 单位换算,加上m或者kb
-		if _, exist := cluster.Allocatable[resInfo.NodeName]; !exist {
-			cluster.Allocatable[resInfo.NodeName] = make(map[string]int64)
-		}
-		//allocatableValue, err := getAllocatableRes(capacity, resInfo)
-		nodeAllocatableRes := cluster.Allocatable[resInfo.NodeName]
-		nodeAllocatableRes[resInfo.ResourceName] = resInfo.Value
-		cluster.Allocatable[resInfo.NodeName] = nodeAllocatableRes
-		// save back single cluster data
-		clusterMap[cName] = cluster
-	}
-	return clusterMap, nil
-}
-
-func slicePages(dataList []ClusterQuotaResponse, pageSize, pageNo int) ([]ClusterQuotaResponse, error) {
-	maxIndex := len(dataList)
-	maxPageSize := (maxIndex + pageSize - 1) / pageSize
-	// pageNo ranges in [1, maxPageSize]
-	pageNo = pageNo - 1
-	if pageNo > maxPageSize || pageNo < 0 {
-		err := fmt.Errorf("list out of range when pageNo %d and pageSize %d", pageNo, pageSize)
-		log.Errorln(err)
-		return dataList, err
-	}
-	startIndex := pageNo * pageSize
-	endIndex := (pageNo + 1) * pageSize
-	if endIndex > maxIndex {
-		endIndex = maxIndex
-	}
-	log.Debugf("dataList number: %d, startIndex: %d, endIndex: %d, maxIndex: %d, maxPageSize: %d",
-		len(dataList), startIndex, endIndex, maxIndex, maxPageSize)
-	dataList = dataList[startIndex:endIndex]
-	return dataList, nil
-}
-
-func updateAllocatable(dataList []ClusterQuotaResponse) ([]ClusterQuotaResponse, error) {
-	cpu := "cpu"
-	memory := "memory"
-	for i, cluster := range dataList {
-		for nodeName, used := range cluster.Allocatable {
-			capacity := cluster.Capacity[nodeName]
-			allocatable := cluster.Allocatable[nodeName]
-			cpuCapacity, err := resource.ParseQuantity(capacity[cpu])
-			if err != nil {
-				log.Errorln(err)
-				return dataList, err
-			}
-			memoryCapacity, err := resource.ParseQuantity(capacity[memory])
-			if err != nil {
-				log.Errorln(err)
-				return dataList, err
-			}
-			// todo GPU
-			cpuAllocatable := cpuCapacity.MilliValue() - used[cpu]
-			memoryAllocatable := memoryCapacity.Value() - used[cpu]
-			// save back
-			allocatable[cpu] = cpuAllocatable
-			allocatable[memory] = memoryAllocatable
-			cluster.Allocatable[nodeName] = allocatable
-		}
-		dataList[i] = cluster
-	}
-	return dataList, nil
 }
--- pkg/storage/cache_pod.go
+++ pkg/storage/cache_pod.go
@@ -18,9 +18,6 @@ package storage
 
 import (
 	"errors"
-	"fmt"
-	"strings"
-
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 
@@ -175,49 +172,6 @@ func (nc *PodResourceCache) UpdateResource(podID string, rName string, podInfo *
 		return tx.Error
 	}
 	return nil
-}
-
-func (nc *PodResourceCache) ListResouces(clusterNameList []string, labels, labelType string) ([]model.ResourceInfoResponse, error) {
-	log.Debugf("begin to list cluster resource.")
-
-	var result []model.ResourceInfoResponse
-	tx := nc.dbCache.Model(&model.ResourceInfo{})
-	tx = tx.Select("`resource_info`.`node_name`, `resource_info`.`resource_name`, " +
-		"sum(`resource_info`.`resource_value`) as resource_value, `n`.`cluster_name`, `n`.`capacity` ").
-		Joins("left join node_info as n on `resource_info`.node_id = `n`.`id` ")
-	switch labelType {
-	case model.ObjectTypeNode:
-		tx = tx.Joins("left join `label_info` as l on resource_info.node_id = `l`.object_id").
-			Where("`l`.object_type=?", labelType)
-	case model.ObjectTypePod:
-		tx = tx.Joins("left join `label_info` as l on resource_info.pod_id = `l`.object_id").
-			Where("`l`.object_type=?", labelType)
-	case "":
-	default:
-		return result, fmt.Errorf("no such label type %s", labelType)
-	}
-	// filter by labels
-	ne := strings.Split(labels, "!=")
-	eq := strings.Split(labels, "=")
-	if labelType != "" && labels != "" {
-		if len(ne) == 2 {
-			tx = tx.Where("`l`.label_name = ? and `l`.label_value <> ?", ne[0], ne[1])
-		} else if len(eq) == 2 {
-			tx = tx.Where("`l`.label_name = ? and `l`.label_value = ?", eq[0], eq[1])
-		}
-	}
-	if len(clusterNameList) != 0 {
-		tx = tx.Where("`n`.`cluster_name` IN ?", clusterNameList)
-	}
-	// group by
-	tx.Group("`resource_info`.node_id, resource_info.resource_value")
-	// limit
-	// submit query
-	if tx.Find(&result); tx.Error != nil {
-		log.Errorf("list resource failed, error:%s", tx.Error)
-		return result, tx.Error
-	}
-	return result, nil
 }
 
 func (nc *PodResourceCache) ListNodeResources(nodeIDList []string) ([]model.ResourceInfo, error) {
