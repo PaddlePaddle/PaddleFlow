94f2cd5b7bf3902a9d9f5a914d9e46c92cbf7ee5
dongzezhao <dongzezhao@baidu.com>
2022-11-17 11:12:27 +0800

    feat(cluster resources): support store shared gpu device index (#918)
    * feat(cluster resources): support store shared gpu device index
    
    * add more ut
    
    * update gpu total count
    
    * add more cases

--- pkg/job/runtime_v2/client/kube_runtime_client.go
+++ pkg/job/runtime_v2/client/kube_runtime_client.go
@@ -344,7 +344,7 @@ func (n *NodeHandler) isExpectedResources(rName string) bool {
 
 func (n *NodeHandler) addQueue(node *corev1.Node, action pfschema.ActionType, labels map[string]string) {
 	capacity := make(map[string]string)
-	for rName, rValue := range node.Status.Capacity {
+	for rName, rValue := range node.Status.Allocatable {
 		resourceName := string(rName)
 		if n.isExpectedResources(resourceName) {
 			capacity[resourceName] = rValue.String()
@@ -377,7 +377,7 @@ func (n *NodeHandler) UpdateNode(old, new interface{}) {
 	newLabels := getLabels(n.labelKeys, newNode.Labels)
 
 	if oldStatus == newStatus &&
-		reflect.DeepEqual(oldNode.Status.Capacity, newNode.Status.Capacity) &&
+		reflect.DeepEqual(oldNode.Status.Allocatable, newNode.Status.Allocatable) &&
 		reflect.DeepEqual(oldLabels, newLabels) {
 		return
 	}
@@ -443,6 +443,10 @@ func convertPodResources(pod *corev1.Pod) map[string]int64 {
 	result := resources.EmptyResource()
 	for _, container := range pod.Spec.Containers {
 		result.Add(k8s.NewResource(container.Resources.Requests))
+	}
+	deviceIDX := k8s.SharedGPUIDX(pod)
+	if deviceIDX > 0 {
+		result.SetResources(k8s.GPUIndexResources, deviceIDX)
 	}
 
 	podResources := make(map[string]int64)
