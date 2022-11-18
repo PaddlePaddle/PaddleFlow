6f9bc820dcae88cddc518c16f0940cd9ed400d45
HaozhengAN <34512369+HaozhengAN@users.noreply.github.com>
2022-11-18 16:00:33 +0800

    [pipeline]: createRun support failureOptions (#924)  Fix bugs that TestRestartEntry fail occasionally (#926) (#931)

--- go-sdk/examples/pipeline/pipeline.go
+++ go-sdk/examples/pipeline/pipeline.go
@@ -23,6 +23,7 @@ import (
 	"github.com/PaddlePaddle/PaddleFlow/go-sdk/service"
 	v1 "github.com/PaddlePaddle/PaddleFlow/go-sdk/service/apiserver/v1"
 	"github.com/PaddlePaddle/PaddleFlow/pkg/common/http/core"
+	"github.com/PaddlePaddle/PaddleFlow/pkg/common/schema"
 )
 
 func GetPipelineFromFile(filepath string) (p *pipeline.Pipeline) {
@@ -54,12 +55,7 @@ func getToken(pfClient *service.PaddleFlowClient) string {
 	return token
 }
 
-func create(pfClient *service.PaddleFlowClient, runYamlRaw, token string) (createResult *v1.CreateRunResponse) {
-	request := &v1.CreateRunRequest{
-		FsName:     "",
-		RunYamlRaw: runYamlRaw,
-	}
-
+func createRun(pfClient *service.PaddleFlowClient, token string, request *v1.CreateRunRequest) (createResult *v1.CreateRunResponse) {
 	createResult, err := pfClient.APIV1().Run().Create(context.TODO(), request, token)
 
 	if err != nil {
@@ -92,13 +88,142 @@ func CreateRunByRunYamlRaw(filepath string) (runID string) {
 
 	token := getToken(pfClient)
 
-	result := create(pfClient, runYamlRaw, token)
+	request := &v1.CreateRunRequest{
+		FsName:     "ppl",
+		RunYamlRaw: runYamlRaw,
+	}
+
+	result := createRun(pfClient, token, request)
 
 	fmt.Println(result)
 	return result.RunID
 }
 
+func CreateRunSpecifyFailureOptions(filepath string, failStrategy string) (runID string) {
+	p, err := pipeline.NewPipelineFromYamlFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	runYamlRaw, err := p.TransToRunYamlRaw()
+	if err != nil {
+		panic(err)
+	}
+
+	config := &core.PaddleFlowClientConfiguration{
+		Host:                       "",
+		Port:                       8999,
+		ConnectionTimeoutInSeconds: 1,
+	}
+	pfClient, err := service.NewForClient(config)
+	if err != nil {
+		panic(err)
+	}
+
+	token := getToken(pfClient)
+
+	request := &v1.CreateRunRequest{
+		FsName:         "ppl",
+		RunYamlRaw:     runYamlRaw,
+		FailureOptions: &schema.FailureOptions{Strategy: failStrategy},
+	}
+
+	result := createRun(pfClient, token, request)
+
+	fmt.Println(result)
+	return result.RunID
+}
+
+func createPipeline(pfClient *service.PaddleFlowClient, token string, request *v1.CreatePipelineRequest) (pplID string) {
+	createResult, err := pfClient.APIV1().Pipeline().Create(context.TODO(), request, token)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("create Run result %v\n", createResult)
+	return
+}
+
+func CreatePipelineByRaw(filepath string) (pplID string) {
+	p, err := pipeline.NewPipelineFromYamlFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	YamlRaw, err := p.TransToRunYamlRaw()
+	if err != nil {
+		panic(err)
+	}
+
+	config := &core.PaddleFlowClientConfiguration{
+		Host:                       "",
+		Port:                       8999,
+		ConnectionTimeoutInSeconds: 1,
+	}
+	pfClient, err := service.NewForClient(config)
+	if err != nil {
+		panic(err)
+	}
+
+	token := getToken(pfClient)
+
+	request := &v1.CreatePipelineRequest{
+		YamlRaw: YamlRaw,
+	}
+
+	result := createPipeline(pfClient, token, request)
+
+	fmt.Println(result)
+	return result
+}
+
+func updatePipeline(pfClient *service.PaddleFlowClient, pipelineID, token string, request *v1.CreatePipelineRequest) (pplID, pplVerID string) {
+	updateResult, err := pfClient.APIV1().Pipeline().Update(context.TODO(), pipelineID, request, token)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("create Run result %v\n", updateResult)
+	return updateResult.PipelineID, updateResult.PipelineVersionID
+}
+
+func UpdatePipelineByRaw(filepath string, pplID string) (string, string) {
+	p, err := pipeline.NewPipelineFromYamlFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	YamlRaw, err := p.TransToRunYamlRaw()
+	if err != nil {
+		panic(err)
+	}
+
+	config := &core.PaddleFlowClientConfiguration{
+		Host:                       "",
+		Port:                       8999,
+		ConnectionTimeoutInSeconds: 1,
+	}
+	pfClient, err := service.NewForClient(config)
+	if err != nil {
+		panic(err)
+	}
+
+	token := getToken(pfClient)
+
+	request := &v1.CreatePipelineRequest{
+		YamlRaw: YamlRaw,
+	}
+
+	pplID, pplVerID := updatePipeline(pfClient, pplID, token, request)
+
+	fmt.Println(pplID, pplVerID)
+	return pplID, pplVerID
+}
+
 func main() {
 	GetPipelineFromFile("")
 	CreateRunByRunYamlRaw("")
+	CreatePipelineByRaw("")
+	UpdatePipelineByRaw("", "ppl-000096")
+	CreateRunSpecifyFailureOptions("", schema.FailureStrategyFailFast)
 }
