6a5b4413899e5f9093d2194b634471aaddd38d44
dongzezhao <dongzezhao@baidu.com>
2022-11-21 13:03:07 +0800

    add test case

--- pkg/job/runtime_v2/controller/job_sync_test.go
+++ pkg/job/runtime_v2/controller/job_sync_test.go
@@ -220,6 +220,9 @@ func TestJobTaskSync(t *testing.T) {
 				JobID:  mockJobID,
 				Status: schema.StatusTaskRunning,
 				Action: schema.Update,
+				PodStatus: &v1.PodStatus{
+					Phase: v1.PodRunning,
+				},
 			},
 		},
 		{
