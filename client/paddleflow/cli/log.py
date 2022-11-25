e8b94623f88e07fc3cc0c54d35668d75bb3e3edc
zhongzichao <zhongzichao@baidu.com>
2022-11-25 11:05:07 +0800

    add log client (#947) (#950)
    * add log client

--- client/paddleflow/cli/log.py
+++ client/paddleflow/cli/log.py
@@ -64,6 +64,43 @@ def show(ctx, runid, jobid=None, pagesize=None, pageno=None, logfileposition=Non
         sys.exit(1)
 
 
+@log.command(context_settings=dict(max_content_width=2000), cls=command_required_option_from_option())
+@click.option('-j', '--jobid', help="jobid and name is one of required field")
+@click.option('-n', '--name', help="name")
+@click.option('-ns', '--namespace', help="namespace")
+@click.option('-c', '--clustername', help="clustername")
+@click.option('-r', '--readfromtail', help="read logs from tail, set value means yes")
+@click.option('-l', '--line_limit', help="line_limit, default is 1000")
+@click.option('-s', '--size_limit', help="size_limit, default is 100MB")
+@click.option('-t', '--type', help="type in deploy or pod")
+@click.option('-f', '--framework', help="job framework")
+@click.pass_context
+def showlimit(ctx, jobid=None, name=None, namespace=None, clustername=None, readfromtail=None, line_limit=None, size_limit=None, type=None, framework=None):
+    """
+    show job,deployment,pod log\n
+    if jobid is null, it would return deployment or pod logs by <namespace, name, type>
+    else it would return PF job logs by jobid
+
+    When both A and B have values, the minimum value is selected to return to the log
+    """
+    client = ctx.obj['client']
+    output_format = 'text'
+    if jobid:
+        click.echo("query job by [jobid]".format(jobid))
+    elif name:
+        click.echo("query log by [namespace/name]".format(namespace, name))
+
+    valid, response = client.show_log_by_limit(job_id=jobid, name=name, namespace=namespace, cluster_name=clustername, read_from_tail=readfromtail,
+                                               line_limit=line_limit, size_limit=size_limit, type=type, framework=framework)
+    if valid:
+        # if jobid is None and len(response['runLog']) > 0:
+        #     response['runLog'] = [response['runLog'][0]]
+        _print_log_by_limit(response, output_format)
+    else:
+        click.echo("show logs failed with message[%s]" % response)
+        sys.exit(1)
+
+
 def _print_run_log(loginfo, out_format):
     """print run log """
     submit_loginfo = loginfo['submitLog']
@@ -82,5 +119,37 @@ def _print_run_log(loginfo, out_format):
         data.append(line)
         data.append(line_log)
         if index != len(run_loginfo) - 1:
+            data.append(['\n'])
+    print_output(data, [], out_format)
+
+def _print_log_by_limit(loginfo, out_format):
+    """print run log by limit """
+    eventList = loginfo['eventList']
+    logs = loginfo['logs']
+
+    if eventList is None or len(eventList) == 0:
+        click.echo(click.style('eventList is None', fg='green'))
+    else:
+        click.echo(click.style('eventList:', fg='green'))
+        for index, item in enumerate(eventList):
+            click.echo(item)
+
+    click.echo(click.style('logs:', fg='green'))
+
+    data = []
+    if len(logs) == 0:
+        data.append(["None"])
+    for index, item in enumerate(logs):
+        if item.job_id:
+            line = ["jobid:" + item.job_id, "taskid:" + item.task_id, "has_next_page:" + str(item.has_next_page),
+                    "truncated:" + str(item.truncated)]
+        else:
+            line = ["name:" + item.name, "taskid:" + item.task_id, "has_next_page:" + str(item.has_next_page),
+                    "line_limit:" + str(item.line_limit), "size_limit:" + str(item.size_limit), "truncated:" + str(item.truncated)]
+        line_log = []
+        line_log.append(item.log_content)
+        data.append(line)
+        data.append(line_log)
+        if index != len(logs) - 1:
             data.append(['\n'])
     print_output(data, [], out_format)
