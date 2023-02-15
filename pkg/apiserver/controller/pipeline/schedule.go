93e9be9c3dd31e495f269bfffd4b78674e219bfb
HaozhengAn <317291885@qq.com>
2023-02-15 20:53:02 +0800

    opt the return code of GetSchedule API while the user has no access to others' schedule

--- pkg/apiserver/controller/pipeline/schedule.go
+++ pkg/apiserver/controller/pipeline/schedule.go
@@ -502,7 +502,6 @@ func StopSchedule(ctx *logger.RequestContext, scheduleID string) error {
 	// check schedule exist && user access right
 	schedule, err := getSchedule(ctx, scheduleID)
 	if err != nil {
-		ctx.ErrorCode = common.ScheduleNotFound
 		err := fmt.Errorf("stop schedule[%s] failed. %s", scheduleID, err.Error())
 		ctx.Logging().Errorf(err.Error())
 		return err
