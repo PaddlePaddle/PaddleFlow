3ef4f7e88b6d6f5fc03a28d440dc37cca0def104
luoyuedong <530122286@qq.com>
2023-02-22 14:24:57 +0800

    fix: cache path mont propagation

--- pkg/fs/csiplugin/mount/pod.go
+++ pkg/fs/csiplugin/mount/pod.go
@@ -306,25 +306,26 @@ func buildMountContainer(mountContainer k8sCore.Container, mountInfo Info) k8sCo
 		},
 	}
 
-	mp := k8sCore.MountPropagationNone
+	m1 := k8sCore.MountPropagationBidirectional
+	m2 := k8sCore.MountPropagationNone
 	volumeMounts := []k8sCore.VolumeMount{
 		{
 			Name:             VolumesKeyMount,
 			MountPath:        schema.FusePodMntDir,
 			SubPath:          mountInfo.FS.ID,
-			MountPropagation: &mp,
+			MountPropagation: &m1,
 		},
 	}
 	if mountInfo.CacheConfig.CacheDir != "" {
 		dataCacheVM := k8sCore.VolumeMount{
 			Name:             VolumesKeyDataCache,
 			MountPath:        FusePodCachePath + DataCacheDir,
-			MountPropagation: &mp,
+			MountPropagation: &m2,
 		}
 		metaCacheVM := k8sCore.VolumeMount{
 			Name:             VolumesKeyMetaCache,
 			MountPath:        FusePodCachePath + MetaCacheDir,
-			MountPropagation: &mp,
+			MountPropagation: &m2,
 		}
 		volumeMounts = append(volumeMounts, dataCacheVM, metaCacheVM)
 	}
