4ad96d1fd19023c96ef17f2a1382dcc3afea0f3a
luoyuedong <530122286@qq.com>
2023-03-27 10:34:38 +0800

    perf: fix fs delete cost long

---
+++ pkg/fs/csiplugin/test_file_test.go
@@ -0,0 +1,22 @@
+package csiplugin
+
+import "testing"
+
+func Test_a(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{
+			name: "test",
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := a(); got != tt.want {
+				t.Errorf("a() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
