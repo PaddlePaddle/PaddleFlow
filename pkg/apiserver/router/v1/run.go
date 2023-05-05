8740a6e26ade3952f7e19d680193e15c4f12ae45
moxianyuan <362129720@qq.com>
2023-05-05 15:36:20 +0800

    cherry-pick from (#1139)：fix bug for unmarshall(default float -> json.Number) (#1142)
    * fix bug for unmarshall(default float -> json.Number)
    
    * fix bug for unmarshall(default float -> json.Number)

--- pkg/apiserver/router/v1/run.go
+++ pkg/apiserver/router/v1/run.go
@@ -68,7 +68,7 @@ func (rr *RunRouter) createRun(w http.ResponseWriter, r *http.Request) {
 	requestId := ctx.RequestID
 	var createRunInfo pipeline.CreateRunRequest
 
-	if err := common.BindJSON(r, &createRunInfo); err != nil {
+	if err := common.BindJSONByDecoder(r, &createRunInfo); err != nil {
 		logger.LoggerForRequest(&ctx).Errorf(
 			"create run failed parsing request body:%+v. error:%s", r.Body, err.Error())
 		common.RenderErrWithMessage(w, ctx.RequestID, ctx.ErrorCode, err.Error())
