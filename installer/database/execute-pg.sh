727aa640d00f443a7ad6ada3468ba7b0d6bdefdf
liujiaxing123666 <2316860375@qq.com>
2023-07-27 20:02:59 +0800

    PGSQL initialization script,paddleflow.sql Supports versions of pgSQL

--- installer/database/execute-pg.sh
+++ installer/database/execute-pg.sh
@@ -7,7 +7,21 @@ export DB_USER=postgres #用户
 export DB_PW= #密码
 export DB_DATABASE=paddleflow #数据库名称
 export PATH=/usr/pgsql-10/bin:/usr/bin; #二进制文件地址
-if [[ $DB_DRIVER = "postgresql" ]];then
+if [ $DB_DRIVER == "mysql" ];then
+  mysql -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT -e "use $DB_DATABASE" &>/dev/null
+  if [ $? -ne 0 ]
+  then
+   echo "MySQL database $DB_DATABASE is not exist"
+  else
+   echo "MySQL database $DB_DATABASE is exist, starting backup."
+   mysqldump -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT --databases $DB_DATABASE >  $DB_DATABASE.bak_`date +%Y%m%d`.sql
+   mysql -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT $DB_DATABASE -e "drop database if exists $DB_DATABASE;"
+  fi
+  echo "creating database $DB_DATABASE."
+  sed -i "s/paddleflow_db/$DB_DATABASE/g" paddleflow.sql
+  mysql -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT -e "source paddleflow.sql"
+  echo "creating database $DB_DATABASE completed."
+elif [[ $DB_DRIVER = "postgres" ]];then
   psql -U $DB_USER -h$DB_HOST -p$DB_PORT --c "\c $DB_DATABASE"  #&>/dev/null
   if [ $? -ne 0 ]
   then
@@ -26,6 +40,6 @@ if [[ $DB_DRIVER = "postgresql" ]];then
   psql -d $DB_DATABASE -U $DB_USER -h$DB_HOST -p$DB_PORT -f /job/paddleflow.sql.bak
   rm -rf /job/paddleflow.sql.bak
   echo "test database $DB_DATABASE."
-elif [[ $DB_DRIVER = "postgres" ]];then
-  echo "not implement"
+else
+  echo 'Database tool filling error, please check!'
 fi
