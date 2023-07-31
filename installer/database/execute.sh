2b78d21ee1ae482b78668a11b2b2cabb878a27f6
liujiaxing123666 <paddleflow>
2023-07-31 14:48:48 +0800

    PGSQL initialization script,paddleflow.sql Supports versions of pgSQL

--- installer/database/execute.sh
+++ installer/database/execute.sh
@@ -12,20 +12,24 @@ if [ $DB_DRIVER == "mysql" ];then
   if [ $? -ne 0 ]
   then
    echo "MySQL database $DB_DATABASE is not exist"
+   mysql -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT -e "CREATE DATABASE IF NOT EXISTS $DB_DATABASE;"
   else
    echo "MySQL database $DB_DATABASE is exist, starting backup."
    mysqldump -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT --databases $DB_DATABASE >  $DB_DATABASE.bak_`date +%Y%m%d`.sql
-   mysql -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT $DB_DATABASE -e "drop database if exists $DB_DATABASE;"
+   mysql -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT -e "drop database if exists $DB_DATABASE;"
+   mysql -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT -e "CREATE DATABASE IF NOT EXISTS $DB_DATABASE;"
   fi
+  cp /job/paddleflow.sql /job/paddleflow.sql.bak
+  sed -i "s/paddleflow_db/$DB_DATABASE/g" /job/paddleflow.sql.bak
   echo "creating database $DB_DATABASE."
-  sed -i "s/paddleflow_db/$DB_DATABASE/g" paddleflow.sql
-  mysql -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT -e "source paddleflow.sql"
-  echo "creating database $DB_DATABASE completed."
-elif [[ $DB_DRIVER = "postgres" ]];then
+  mysql -u$DB_USER -h$DB_HOST -p$DB_PW -P$DB_PORT $DB_DATABASE -e "source /job/paddleflow.sql.bak"
+  rm -rf /job/paddleflow.sql.bak
+  echo "test database $DB_DATABASE."
+elif [[ $DB_DRIVER == "postgresql" ]];then
   psql -U $DB_USER -h$DB_HOST -p$DB_PORT --c "\c $DB_DATABASE"  #&>/dev/null
   if [ $? -ne 0 ]
   then
-   echo "PostgreSQL database $DB_DATABASE is not exist"
+   echo "MySQL database $DB_DATABASE is not exist"
      psql -U $DB_USER -h$DB_HOST -p$DB_PORT --c "CREATE DATABASE $DB_DATABASE"
     echo 'Create Database...'
   else
@@ -34,12 +38,10 @@ elif [[ $DB_DRIVER = "postgres" ]];then
    psql -U $DB_USER -h$DB_HOST -p$DB_PORT --c "DROP DATABASE $DB_DATABASE"
    psql -U $DB_USER -h$DB_HOST -p$DB_PORT --c "CREATE DATABASE $DB_DATABASE"
   fi
-  cp /job/paddleflow.sql /job/paddleflow.sql.bak
-  sed -i "s/paddleflow_db/$DB_DATABASE/g" /job/paddleflow.sql.bak
+  cp /job/paddleflow-pg.sql /job/paddleflow-pg.sql.bak
+  sed -i "s/paddleflow_db/$DB_DATABASE/g" /job/paddleflow-pg.sql.bak
   echo "creating database $DB_DATABASE."
-  psql -d $DB_DATABASE -U $DB_USER -h$DB_HOST -p$DB_PORT -f /job/paddleflow.sql.bak
+  psql -d $DB_DATABASE -U $DB_USER -h$DB_HOST -p$DB_PORT -f /job/paddleflow-pg.sql.bak
   rm -rf /job/paddleflow.sql.bak
   echo "test database $DB_DATABASE."
-else
-  echo 'Database tool filling error, please check!'
 fi
