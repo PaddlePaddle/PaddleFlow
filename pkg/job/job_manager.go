9a1ab9237be54f93f13f55603835f6c9bda1f2b3
dongzezhao <dongzezhao@baidu.com>
2023-09-12 19:28:37 +0800

    feat(job): remove useless code (#1240)
    * feat(job): remove useless code
    
    * remove ListQueueInitJob api

--- pkg/job/job_manager.go
+++ pkg/job/job_manager.go
@@ -46,15 +46,10 @@ type QueueJobsFunc func(string, []schema.JobStatus) []model.Job
 
 type JobManagerImpl struct {
 	// activeClusters is a method for listing active clusters from db
-	activeClusters ActiveClustersFunc
-	// activeQueueJobs is a method for listing jobs on active queue
-	// deprecated
-	activeQueueJobs QueueJobsFunc
+	activeClusters  ActiveClustersFunc
 	queueExpireTime time.Duration
 	queueCache      gcache.Cache
-
-	listQueueInitJobs func(string) []model.Job
-	jobLoopPeriod     time.Duration
+	jobLoopPeriod   time.Duration
 
 	// jobQueues contains JobQueue for jobs in queue
 	jobQueues api.JobQueues
@@ -96,10 +91,8 @@ func (m *JobManagerImpl) init() {
 	m.clusterSyncPeriod = time.Duration(clusterSyncTime) * time.Second
 }
 
-func (m *JobManagerImpl) Start(activeClusters ActiveClustersFunc, activeQueueJobs QueueJobsFunc) {
+func (m *JobManagerImpl) Start(activeClusters ActiveClustersFunc) {
 	m.activeClusters = activeClusters
-	m.activeQueueJobs = activeQueueJobs
-	m.listQueueInitJobs = storage.Job.ListQueueInitJob
 	/// init config for job manager
 	m.init()
 	// start job manager
